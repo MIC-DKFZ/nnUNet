6b5419a57023c0ca8b909c491c55d569ed886baf
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2019-04-25 11:03:19 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,22 @@ from setuptools import setup
 setup(name='nnunet',
       version='0.1',
       description='no new-net. Framework for out-of-the box medical image segmentation.',
-      url='none',
-      author='Fabian Isensee',
+      url='https://github.com/MIC-DKFZ/nnUNet',
+      author='Division of Medical Image Computing, German Cancer Research Center',
       author_email='f.isensee@dkfz-heidelberg.de',
-      license='private - fck off',
-      zip_safe=False)
+      license='Apache License Version 2.0, January 2004',
+      install_requires=[
+            "tqdm",
+            "dicom2nifti",
+            "scikit-image>=0.14",
+            "medpy",
+            "scipy",
+            "batchgenerators>=0.19.3",
+            "numpy",
+            "sklearn",
+            "SimpleITK",
+            "pandas",
+      ],
+      keywords=['deep learning', 'image segmentation', 'medical image analysis',
+                'medical image segmentation', 'nnU-Net', 'nnunet']
+      )
