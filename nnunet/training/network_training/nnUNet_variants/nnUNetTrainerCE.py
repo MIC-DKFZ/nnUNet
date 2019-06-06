4d28b0f9f4acc7204b6b1baf20e57ac0d7774244
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2019-06-06 13:51:54 +0200

    nnunet crossentropy loss trainer

---
+++ nnunet/training/network_training/nnUNet_variants/nnUNetTrainerCE.py
@@ -0,0 +1,10 @@
+from nnunet.training.loss_functions.dice_loss import CrossentropyND
+from nnunet.training.network_training.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainerCE(nnUNetTrainer):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super(nnUNetTrainerCE, self).__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
+                                              unpack_data, deterministic, fp16)
+        self.loss = CrossentropyND()
