dbb2fb5e9bd70182feb5589db34b798eeba54e71
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2019-06-27 10:44:25 +0200

    added TopK loss and DC_and_topk_loss

---
+++ nnunet/training/loss_functions/TopK_loss.py
@@ -0,0 +1,33 @@
+#    Copyright 2019 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import numpy as np
+import torch
+from nnunet.training.loss_functions.ND_Crossentropy import CrossentropyND
+
+
+class TopKLoss(CrossentropyND):
+    """
+    Network has to have NO LINEARITY!
+    """
+    def __init__(self, weight=None, ignore_index=-100, k=10):
+        self.k = k
+        super(TopKLoss, self).__init__(weight, False, ignore_index, reduce=False)
+
+    def forward(self, inp, target):
+        target = target[:, 0].long()
+        res = super(TopKLoss, self).forward(inp, target)
+        num_voxels = np.prod(res.shape)
+        res, _ = torch.topk(res.view((-1, )), int(num_voxels * self.k / 100), sorted=False)
+        return res.mean()
--- nnunet/training/loss_functions/dice_loss.py
+++ nnunet/training/loss_functions/dice_loss.py
@@ -12,6 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 import torch
+from nnunet.training.loss_functions.TopK_loss import TopKLoss
 from nnunet.utilities.nd_softmax import softmax_helper
 from nnunet.training.loss_functions.ND_Crossentropy import CrossentropyND
 from nnunet.utilities.tensor_utilities import sum_tensor
@@ -124,4 +125,21 @@ class DC_and_CE_loss(nn.Module):
             result = ce_loss + dc_loss
         else:
             raise NotImplementedError("nah son") # reserved for other stuff (later)
+        return result
+
+
+class DC_and_topk_loss(nn.Module):
+    def __init__(self, soft_dice_kwargs, ce_kwargs, aggregate="sum"):
+        super(DC_and_topk_loss, self).__init__()
+        self.aggregate = aggregate
+        self.ce = TopKLoss(**ce_kwargs)
+        self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+
+    def forward(self, net_output, target):
+        dc_loss = self.dc(net_output, target)
+        ce_loss = self.ce(net_output, target)
+        if self.aggregate == "sum":
+            result = ce_loss + dc_loss
+        else:
+            raise NotImplementedError("nah son") # reserved for other stuff (later?)
         return result
