006a8f77a6ca9d6f93d58841f89b56f70b90ca79
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2020-02-26 15:12:12 +0100

    lower init lr for mcc loss

--- nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_MCC.py
+++ nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_MCC.py
@@ -8,6 +8,7 @@ class nnUNetTrainerV2_Loss_MCC(nnUNetTrainerV2):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
+        self.initial_lr = 1e-3
         self.loss = MCCLoss(apply_nonlin=softmax_helper, batch_mcc=self.batch_dice, do_bg=True)
 
 
@@ -16,5 +17,6 @@ class nnUNetTrainerV2_Loss_MCCnoBG(nnUNetTrainerV2):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
+        self.initial_lr = 1e-3
         self.loss = MCCLoss(apply_nonlin=softmax_helper, batch_mcc=self.batch_dice, do_bg=False)
 
