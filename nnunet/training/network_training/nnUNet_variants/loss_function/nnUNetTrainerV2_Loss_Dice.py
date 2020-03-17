ad3fcb3c5272963b79c82512a298f017283b7939
Fabian Isensee <isenseef@gmail.com>
2020-03-17 10:38:27 +0100

    nnUNetTrainerV2_Loss_DicewithBG_LR1en3

--- nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_Dice.py
+++ nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_Dice.py
@@ -10,6 +10,7 @@ class nnUNetTrainerV2_Loss_Dice(nnUNetTrainerV2):
                          deterministic, fp16)
         self.loss = SoftDiceLoss(**{'apply_nonlin': softmax_helper, 'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False})
 
+
 class nnUNetTrainerV2_Loss_DicewithBG(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
--- nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_Dice_lr1en3.py
+++ nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_Dice_lr1en3.py
@@ -1,5 +1,5 @@
 from nnunet.training.network_training.nnUNet_variants.loss_function.nnUNetTrainerV2_Loss_Dice import \
-    nnUNetTrainerV2_Loss_Dice
+    nnUNetTrainerV2_Loss_Dice, nnUNetTrainerV2_Loss_DicewithBG
 
 
 class nnUNetTrainerV2_Loss_Dice_LR1en3(nnUNetTrainerV2_Loss_Dice):
@@ -8,3 +8,12 @@ class nnUNetTrainerV2_Loss_Dice_LR1en3(nnUNetTrainerV2_Loss_Dice):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
         self.initial_lr = 1e-3
+
+
+class nnUNetTrainerV2_Loss_DicewithBG_LR1en3(nnUNetTrainerV2_Loss_DicewithBG):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.initial_lr = 1e-3
+
