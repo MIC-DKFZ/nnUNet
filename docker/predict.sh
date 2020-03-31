a2ed5e26615ccb3043ae26f2ec98bec910e2654f
Fabian Isensee <isenseef@gmail.com>
2020-03-31 16:00:40 +0200

    removed docker because it was deprecated, TODO needs updating

--- docker/predict.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo '==============================='
-echo 'Run nnUnet Prediction'
-echo '==============================='
-cd nnUNet/nnunet
-# Change the call according to your parameters.
-python3 inference/predict_simple.py -i $INPUTDIR -o $OUTPUTDIR -t $TASK_NAME -tr nnUNetTrainer -m 3d_fullres
