d9fa6cefcdfdf76fee8f4ac9db615cdbb13c94e0
Fabian Isensee <isenseef@gmail.com>
2020-04-03 10:47:25 +0200

    update paper reference

--- nnunet/__init__.py
+++ nnunet/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
-print("\nPlease cite the following paper when using nnUNet:\nIsensee, Fabian, et al. \"nnU-Net: Breaking the Spell on "
-      "Successful Medical Image Segmentation.\" arXiv preprint arXiv:1904.08128 (2019).")
+print("\nPlease cite the following paper when using nnUNet:\nFabian Isensee, Paul F. Jäger, Simon A. A. Kohl, "
+      "Jens Petersen, Klaus H. Maier-Hein \"Automated Design of Deep Learning Methods for Biomedical Image "
+      "Segmentation\" arXiv preprint arXiv:1904.08128 (2020).")
 print("If you have questions or suggestions, feel free to open an issue at https://github.com/MIC-DKFZ/nnUNet\n")
 
 from . import *
