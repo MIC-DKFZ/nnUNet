fe9934c91bc583571e7dd6f7bb388b9d27560fd8
Fabian Isensee <isenseef@gmail.com>
2020-04-07 15:27:10 +0200

    add brats 2018, add brats 2019 validation data

--- nnunet/dataset_conversion/Task032_BraTS_2018.py
+++ nnunet/dataset_conversion/Task032_BraTS_2018.py
@@ -17,29 +17,10 @@ import numpy as np
 from collections import OrderedDict
 
 from batchgenerators.utilities.file_and_folder_operations import *
+from nnunet.dataset_conversion.Task043_BraTS_2019 import copy_BraTS_segmentation_and_convert_labels
 from nnunet.paths import nnUNet_raw_data
 import SimpleITK as sitk
 import shutil
-
-
-def copy_BraTS_segmentation_and_convert_labels(in_file, out_file):
-    # use this for segmentation only!!!
-    # nnUNet wants the labels to be continuous. BraTS is 0, 1, 2, 4 -> we make that into 0, 1, 2, 3
-    img = sitk.ReadImage(in_file)
-    img_npy = sitk.GetArrayFromImage(img)
-
-    uniques = np.unique(img_npy)
-    for u in uniques:
-        if u not in [0, 1, 2, 4]:
-            raise RuntimeError('unexpected label')
-
-    seg_new = np.zeros_like(img_npy)
-    seg_new[img_npy == 4] = 3
-    seg_new[img_npy == 2] = 1
-    seg_new[img_npy == 1] = 2
-    img_corr = sitk.GetImageFromArray(seg_new)
-    img_corr.CopyInformation(img)
-    sitk.WriteImage(img_corr, out_file)
 
 
 if __name__ == "__main__":
@@ -47,8 +28,8 @@ if __name__ == "__main__":
     REMEMBER TO CONVERT LABELS BACK TO BRATS CONVENTION AFTER PREDICTION!
     """
 
-    task_name = "Task043_BraTS2019"
-    downloaded_data_dir = "/home/fabian/data/BraTS2019/MICCAI_BraTS_2019_Data_Training"
+    task_name = "Task032_BraTS2018"
+    downloaded_data_dir = "/home/fabian/Downloads/BraTS2018_train_val_test_data/MICCAI_BraTS_2018_Data_Training"
 
     target_base = join(nnUNet_raw_data, task_name)
     target_imagesTr = join(target_base, "imagesTr")
@@ -89,12 +70,11 @@ if __name__ == "__main__":
 
             copy_BraTS_segmentation_and_convert_labels(seg, join(target_labelsTr, patient_name + ".nii.gz"))
 
-
     json_dict = OrderedDict()
-    json_dict['name'] = "BraTS2019"
+    json_dict['name'] = "BraTS2018"
     json_dict['description'] = "nothing"
     json_dict['tensorImageSize'] = "4D"
-    json_dict['reference'] = "see BraTS2019"
+    json_dict['reference'] = "see BraTS2018"
     json_dict['licence'] = "see BraTS2019 license"
     json_dict['release'] = "0.0"
     json_dict['modality'] = {
@@ -117,7 +97,8 @@ if __name__ == "__main__":
 
     save_json(json_dict, join(target_base, "dataset.json"))
 
-    downloaded_data_dir = "/home/fabian/Downloads/BraTS2018_train_val_test_data/MICCAI_BraTS_2019_Data_Validation"
+    del tpe, cur
+    downloaded_data_dir = "/home/fabian/Downloads/BraTS2018_train_val_test_data/MICCAI_BraTS_2018_Data_Validation"
 
     for p in subdirs(downloaded_data_dir, join=False):
         patdir = join(downloaded_data_dir, p)
@@ -139,8 +120,6 @@ if __name__ == "__main__":
         shutil.copy(t2, join(target_imagesVal, patient_name + "_0002.nii.gz"))
         shutil.copy(flair, join(target_imagesVal, patient_name + "_0003.nii.gz"))
 
-    """
-    #I dont have the testing data
     downloaded_data_dir = "/home/fabian/Downloads/BraTS2018_train_val_test_data/MICCAI_BraTS_2018_Data_Testing_FIsensee"
 
     for p in subdirs(downloaded_data_dir, join=False):
@@ -161,4 +140,4 @@ if __name__ == "__main__":
         shutil.copy(t1, join(target_imagesTs, patient_name + "_0000.nii.gz"))
         shutil.copy(t1c, join(target_imagesTs, patient_name + "_0001.nii.gz"))
         shutil.copy(t2, join(target_imagesTs, patient_name + "_0002.nii.gz"))
-        shutil.copy(flair, join(target_imagesTs, patient_name + "_0003.nii.gz"))"""
+        shutil.copy(flair, join(target_imagesTs, patient_name + "_0003.nii.gz"))
