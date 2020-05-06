648428843df96ab8966277c7b5536c5b1719cca0
Prateek Gupta <prateek.gupta891@gmail.com>
2020-05-06 18:12:26 +0530

    updated path for colab implementation

--- nnunet/paths.py
+++ nnunet/paths.py
@@ -26,9 +26,13 @@ default_cascade_trainer = "nnUNetTrainerV2CascadeFullRes"
 PLEASE READ paths.md FOR INFORMATION TO HOW TO SET THIS UP
 """
 
-base = os.environ['nnUNet_raw_data_base'] if "nnUNet_raw_data_base" in os.environ.keys() else None
-preprocessing_output_dir = os.environ['nnUNet_preprocessed'] if "nnUNet_preprocessed" in os.environ.keys() else None
-network_training_output_dir_base = os.path.join(os.environ['RESULTS_FOLDER']) if "RESULTS_FOLDER" in os.environ.keys() else None
+# base = os.environ['nnUNet_raw_data_base'] if "nnUNet_raw_data_base" in os.environ.keys() else None
+# preprocessing_output_dir = os.environ['nnUNet_preprocessed'] if "nnUNet_preprocessed" in os.environ.keys() else None
+# network_training_output_dir_base = os.path.join(os.environ['RESULTS_FOLDER']) if "RESULTS_FOLDER" in os.environ.keys() else None
+
+base = './nnUNet_raw_data_base'
+preprocessing_output_dir = './nnUNet_preprocessed'
+network_training_output_dir_base = './nnUNet_trained_models'
 
 if base is not None:
     nnUNet_raw_data = join(base, "nnUNet_raw_data")
