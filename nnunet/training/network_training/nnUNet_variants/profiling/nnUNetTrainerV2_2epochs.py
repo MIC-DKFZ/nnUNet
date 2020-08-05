10e9fea1ed8150d53748648f8eb88ced33747bac
Fabian Isensee <isenseef@gmail.com>
2020-08-05 19:47:28 +0200

    more benchmarking

--- nnunet/training/network_training/nnUNet_variants/profiling/nnUNetTrainerV2_2epochs.py
+++ nnunet/training/network_training/nnUNet_variants/profiling/nnUNetTrainerV2_2epochs.py
@@ -13,8 +13,18 @@
 #    limitations under the License.
 from typing import Tuple
 import numpy as np
+import torch
 from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
 from nnunet.training.network_training.nnUNetTrainerV2_DDP import nnUNetTrainerV2_DDP
+from nnunet.training.network_training.nnUNet_variants.architectural_variants.nnUNetTrainerV2_noDeepSupervision import \
+    nnUNetTrainerV2_noDeepSupervision
+from nnunet.utilities.to_torch import maybe_to_torch, to_cuda
+from torch import nn
+from torch.nn.utils import clip_grad_norm_
+try:
+    from apex import amp
+except ImportError:
+    amp = None
 
 
 class nnUNetTrainerV2_2epochs(nnUNetTrainerV2):
@@ -65,6 +75,139 @@ class nnUNetTrainerV2_5epochs(nnUNetTrainerV2):
 
     def save_checkpoint(self, fname, save_optimizer=True):
         pass
+
+
+class nnUNetTrainerV2_5epochs_CEnoDS(nnUNetTrainerV2_noDeepSupervision):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.max_num_epochs = 5
+        self.loss = nn.CrossEntropyLoss() # CrossentropyND()
+
+    def validate(self, do_mirroring: bool = True, use_sliding_window: bool = True, step_size: float = 0.5,
+                 save_softmax: bool = True, use_gaussian: bool = True, overwrite: bool = True,
+                 validation_folder_name: str = 'validation_raw', debug: bool = False, all_in_gpu: bool = False,
+                 segmentation_export_kwargs=None):
+        pass
+
+    def predict_preprocessed_data_return_seg_and_softmax(self, data: np.ndarray, do_mirroring: bool = True,
+                                                         mirror_axes: Tuple[int] = None, use_sliding_window: bool = True,
+                                                         step_size: float = 0.5, use_gaussian: bool = True,
+                                                         pad_border_mode: str = 'constant', pad_kwargs: dict = None,
+                                                         all_in_gpu: bool = True,
+                                                         verbose: bool = True) -> Tuple[np.ndarray, np.ndarray]:
+        pass
+
+    def save_checkpoint(self, fname, save_optimizer=True):
+        pass
+
+    def run_iteration(self, data_generator, do_backprop=True, run_online_evaluation=False):
+        data_dict = next(data_generator)
+        data = data_dict['data']
+        target = data_dict['target']
+
+        data = maybe_to_torch(data)
+        target = maybe_to_torch(target)
+
+        if torch.cuda.is_available():
+            data = to_cuda(data)
+            target = to_cuda(target)
+
+        self.optimizer.zero_grad()
+
+        output = self.network(data)
+
+        del data
+        target = target.long()[:, 0]
+        loss = self.loss(output, target)
+
+        if run_online_evaluation:
+            self.run_online_evaluation(output, target)
+        del target
+
+        if do_backprop:
+            if not self.fp16 or amp is None or not torch.cuda.is_available():
+                loss.backward()
+            else:
+                with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            _ = clip_grad_norm_(self.network.parameters(), 12)
+            self.optimizer.step()
+
+        return loss.detach().cpu().numpy()
+
+    def run_online_evaluation(self, output, target):
+        pass
+
+    def finish_online_evaluation(self):
+        pass
+
+
+class nnUNetTrainerV2_5epochs_noDS(nnUNetTrainerV2_noDeepSupervision):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.max_num_epochs = 5
+
+    def validate(self, do_mirroring: bool = True, use_sliding_window: bool = True, step_size: float = 0.5,
+                 save_softmax: bool = True, use_gaussian: bool = True, overwrite: bool = True,
+                 validation_folder_name: str = 'validation_raw', debug: bool = False, all_in_gpu: bool = False,
+                 segmentation_export_kwargs=None):
+        pass
+
+    def predict_preprocessed_data_return_seg_and_softmax(self, data: np.ndarray, do_mirroring: bool = True,
+                                                         mirror_axes: Tuple[int] = None, use_sliding_window: bool = True,
+                                                         step_size: float = 0.5, use_gaussian: bool = True,
+                                                         pad_border_mode: str = 'constant', pad_kwargs: dict = None,
+                                                         all_in_gpu: bool = True,
+                                                         verbose: bool = True) -> Tuple[np.ndarray, np.ndarray]:
+        pass
+
+    def save_checkpoint(self, fname, save_optimizer=True):
+        pass
+
+    def run_iteration(self, data_generator, do_backprop=True, run_online_evaluation=False):
+        data_dict = next(data_generator)
+        data = data_dict['data']
+        target = data_dict['target']
+
+        data = maybe_to_torch(data)
+        target = maybe_to_torch(target)
+
+        if torch.cuda.is_available():
+            data = to_cuda(data)
+            target = to_cuda(target)
+
+        self.optimizer.zero_grad()
+
+        output = self.network(data)
+
+        del data
+        loss = self.loss(output, target)
+
+        if run_online_evaluation:
+            self.run_online_evaluation(output, target)
+        del target
+
+        if do_backprop:
+            if not self.fp16 or amp is None or not torch.cuda.is_available():
+                loss.backward()
+            else:
+                with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            _ = clip_grad_norm_(self.network.parameters(), 12)
+            self.optimizer.step()
+
+        return loss.detach().cpu().numpy()
+
+    def run_online_evaluation(self, output, target):
+        pass
+
+    def finish_online_evaluation(self):
+        pass
+
 
 
 class nnUNetTrainerV2_DDP_5epochs(nnUNetTrainerV2_DDP):
