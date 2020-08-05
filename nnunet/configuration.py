da8469e8ddfb263f3876fbc0f28529900f030bbf
Fabian Isensee <isenseef@gmail.com>
2020-08-05 08:37:15 +0200

    bugfix

--- nnunet/configuration.py
+++ nnunet/configuration.py
@@ -1,5 +1,5 @@
 import os
 
-default_num_threads = 8 if 'nnUNet_def_n_proc' not in os.environ else os.environ['nnUNet_def_n_proc']
+default_num_threads = 8 if 'nnUNet_def_n_proc' not in os.environ else int(os.environ['nnUNet_def_n_proc'])
 RESAMPLING_SEPARATE_Z_ANISO_THRESHOLD = 3  # determines what threshold to use for resampling the low resolution axis
 # separately (with NN)
--- nnunet/training/data_augmentation/default_data_augmentation.py
+++ nnunet/training/data_augmentation/default_data_augmentation.py
@@ -84,7 +84,7 @@ default_3D_augmentation_params = {
     "additive_brightness_mu": 0.0,
     "additive_brightness_sigma": 0.1,
 
-    "num_threads": 12 if 'nnUNet_n_proc_DA' not in os.environ else os.environ['nnUNet_n_proc_DA'],
+    "num_threads": 12 if 'nnUNet_n_proc_DA' not in os.environ else int(os.environ['nnUNet_n_proc_DA']),
     "num_cached_per_thread": 1,
 }
 
