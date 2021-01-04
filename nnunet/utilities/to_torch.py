3b96a10847459262ef11b833d11866dd8f4bd323
Fabian Isensee <isenseef@gmail.com>
2021-01-04 12:43:02 +0100

    non_blocking was not passed along properly in to_cuda. This did not affect any training because non_blocking was set to True anyways

--- nnunet/utilities/to_torch.py
+++ nnunet/utilities/to_torch.py
@@ -27,5 +27,5 @@ def to_cuda(data, non_blocking=True, gpu_id=0):
     if isinstance(data, list):
         data = [i.cuda(gpu_id, non_blocking=non_blocking) for i in data]
     else:
-        data = data.cuda(gpu_id, non_blocking=True)
+        data = data.cuda(gpu_id, non_blocking=non_blocking)
     return data
