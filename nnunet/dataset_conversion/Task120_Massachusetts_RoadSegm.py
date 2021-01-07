dc6d38128368a9beb5781ce23a4fd3f4835a24d2
Fabian Isensee <isenseef@gmail.com>
2021-01-07 16:03:52 +0100

    continued on Task120_MassRoadsSeg example

--- nnunet/dataset_conversion/Task120_Massachusetts_RoadSegm.py
+++ nnunet/dataset_conversion/Task120_Massachusetts_RoadSegm.py
@@ -1,6 +1,7 @@
+import numpy as np
 from batchgenerators.utilities.file_and_folder_operations import *
 from nnunet.dataset_conversion.utils import generate_dataset_json
-from nnunet.paths import nnUNet_raw_data
+from nnunet.paths import nnUNet_raw_data, preprocessing_output_dir
 from nnunet.utilities.file_conversions import convert_2d_image_to_nifti
 
 if __name__ == '__main__':
@@ -82,16 +83,96 @@ if __name__ == '__main__':
     generate_dataset_json(join(target_base, 'dataset.json'), target_imagesTr, target_imagesTs, ('Red', 'Green', 'Blue'),
                           labels={1: 'street'}, dataset_name=task_name, license='hands off!')
 
-    # once this is completed, you can use the dataset like any other nnU-Net dataset. Note that since this is a 2D
-    # dataset there is no need to run preprocessing for 3D U-Nets. You should therefore run the
-    # `nnUNet_plan_and_preprocess` command like this:
+    """
+    once this is completed, you can use the dataset like any other nnU-Net dataset. Note that since this is a 2D
+    dataset there is no need to run preprocessing for 3D U-Nets. You should therefore run the 
+    `nnUNet_plan_and_preprocess` command like this:
+    
+    > nnUNet_plan_and_preprocess -t 120 -pl3d None
+    
+    once that is completed, you can run the trainings as follows:
+    > nnUNet_train 2d nnUNetTrainerV2 120 FOLD
+    
+    (where fold is again 0, 1, 2, 3 and 4 - 5-fold cross validation)
+    
+    there is no need to run nnUNet_find_best_configuration because there is only one model to shoose from.
+    Note that without running nnUNet_find_best_configuration, nnU-Net will not have determined a postprocessing
+    for the whole cross-validation. Spoiler: it will determine not to run postprocessing anyways. If you are using
+    a different 2D dataset, you can make nnU-Net determine the postprocessing by using the
+    `nnUNet_determine_postprocessing` command
+    """
 
-    # nnUNet_plan_and_preprocess -t 120 -pl3d None
+    #############################################################################################################
 
-    # once that is completed, you can run the trainings as follows:
+    """
+    THIS PART IS OPTIONAL! It requires you to have run nnUNet_plan_and_preprocess.
+    This section is purely here to demonstrate how you can manually edit the plans files to approach 
+    a segmentation problem with a different patch size, batch size and U-Net architecture
+    
+    The output of `nnUNet_plan_and_preprocess` for this task looks like this (shortened!):
+    
+    [{'batch_size': 2, 'num_pool_per_axis': [8, 8], 'patch_size': array([1280, 1024]), ...]
+    
+    This means that nnU-Net intends to use a patch size of 1280x1024 and a U-Net architecture with 8 pooling 
+    operations per axis. Due to GPU memory constraints, the batch size is just 2.
+        
+    Knowing the dataset we could hypothesize that a different approach might produce better results: The decision 
+    of whether a pixel belongs to 'road' or not does not depent on the large contextual information that the large 
+    patch size (and U-Net architecture) offer and could instead be made with more local information. Also, training with
+    a batch size of just 2 in a dataset with 800 training cases means that each batch contains only limited variability.
+    So one possible conclusion could be that smaller patches but larger patch sizes might result in a better 
+    segmentation outcome. Let's investigate (using the same GPU memory constraint!):
+    """
 
-    # there is no need to run nnUNet_find_best_configuration because there is only one model to shoose from.
-    # Note that without running nnUNet_find_best_configuration, nnU-Net will not have determined a postprocessing
-    # for the whole cross-validation. Spoiler: it will determine not to run postprocessing anyways. If you are using
-    # a different 2D dataset, you can make nnU-Net determine the postprocessing by using the
-    # `nnUNet_determine_postprocessing` command
+    """
+    Variant 1: patch size 512x512, batch size 12
+    The following snippet makes the necessary adaptations to the plans file
+    """
+    plans_fname = join(preprocessing_output_dir, task_name, 'nnUNetPlansv2.1_plans_2D.pkl')
+    plans = load_pickle(plans_fname)
+    plans['plans_per_stage'][0]['batch_size'] = 12
+    plans['plans_per_stage'][0]['patch_size'] = np.array((512, 512))
+    plans['plans_per_stage'][0]['num_pool_per_axis'] = [7, 7]
+    # because we changed the num_pool_per_axis, we need to change conv_kernel_sizes and pool_op_kernel_sizes as well!
+    plans['plans_per_stage'][0]['pool_op_kernel_sizes'] = [[2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2]]
+    plans['plans_per_stage'][0]['conv_kernel_sizes'] = [[3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3]]
+    # for a network with num_pool_per_axis [7,7] the correct length of pool kernel sizes is 7 and the length of conv
+    # kernel sizes is 8! Note that you can also change these nubers if you believe it makes sense. A pool kernel size
+    # of 1 will result in no pooling along that axis, a kernel size of 3 will reduce the size of the feature map
+    # representations by factor 3 instead of 2.
+
+    # save the plans under a new plans name. Note that the new plans file must end with _plans_2D.pkl!
+    save_pickle(plans, join(preprocessing_output_dir, task_name, 'nnUNetPlansv2.1_ps512_bs12_plans_2D.pkl'))
+
+    """
+    Variant 2: patch size 256x256, batch size 48
+    """
+    plans = load_pickle(plans_fname)
+    plans['plans_per_stage'][0]['batch_size'] = 60
+    plans['plans_per_stage'][0]['patch_size'] = np.array((256, 256))
+    plans['plans_per_stage'][0]['num_pool_per_axis'] = [6, 6]
+    plans['plans_per_stage'][0]['pool_op_kernel_sizes'] = [[2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2]]
+    plans['plans_per_stage'][0]['conv_kernel_sizes'] = [[3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3]]
+    save_pickle(plans, join(preprocessing_output_dir, task_name, 'nnUNetPlansv2.1_ps256_bs60_plans_2D.pkl'))
+
+    """
+    You can now use these custom plans files to train the networks and compare the results! Remeber that all nnUNet_* 
+    commands have the -h argument to display their arguments. nnUNet_train supports custom plans via the -p argument. 
+    Custom plans must be the prefix, so here this is everything except '_plans_2D.pkl':
+    
+    Variant 1:
+    > nnUNet_train 2d nnUNetTrainerV2 120 FOLD -p nnUNetPlansv2.1_ps512_bs12
+    
+    Variant 2:
+    > nnUNet_train 2d nnUNetTrainerV2 120 FOLD -p nnUNetPlansv2.1_ps256_bs48
+    
+    Let all 5 folds run for each plans file (original and the two variants). To compare the results, you can make use of
+    nnUNet_determine_postprocessing to get the necessary metrics 
+    
+    (TO BE CONTINUED)
+    """
+
+
+    """
+    we could also add another experiment planner for downsampled images
+    """
