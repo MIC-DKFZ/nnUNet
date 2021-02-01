c379974dbca57d2d1f4b999128de293b778d1d52
Fabian Isensee <f.isensee@dkfz.de>
2021-02-01 11:33:48 +0100

    updated task005 pretrained weights

--- nnunet/inference/pretrained_models/download_pretrained_model.py
+++ nnunet/inference/pretrained_models/download_pretrained_model.py
@@ -11,17 +11,13 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-import shutil
-import tempfile
+from subprocess import call
 from time import time
-from urllib.request import urlopen
 
+import requests
 from batchgenerators.utilities.file_and_folder_operations import join, isfile
 
 from nnunet.paths import network_training_output_dir
-from subprocess import call
-import requests
-import os
 
 
 def get_available_models():
@@ -59,7 +55,7 @@ def get_available_models():
                            "Segmentation targets are peripheral and central zone, \n"
                            "input modalities are 0: T2, 1: ADC. \n"
                            "Also see Medical Segmentation Decathlon, http://medicaldecathlon.com/",
-            'url': "https://zenodo.org/record/4003545/files/Task005_Prostate.zip?download=1"
+            'url': "https://zenodo.org/record/4485926/files/Task005_Prostate.zip?download=1"
         },
         "Task006_Lung": {
             'description': "Lung Nodule Segmentation. \n"
