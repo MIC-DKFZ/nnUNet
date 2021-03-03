973a44e1e3a6838d9806d8974e5c57ddaf11767a
Karol <KarolGotkowski@gmx.de>
2021-03-03 14:50:18 +0100

    Small fix

--- medseg/tmp_rename.py
+++ medseg/tmp_rename.py
@@ -6,6 +6,4 @@ filenames = utils.load_filenames(path)
 
 index = 84
 for filename in filenames:
-    if "covid" in os.path.basename(filename[:-7]):  # medseg: len(os.path.basename(filename[:-7])) == 1, all other like: "study" in os.path.basename(filename[:-7])
-        os.rename(filename, path + str(index).zfill(4) + "_0000.nii.gz")
-        index += 1
+    os.rename(filename, path + str(index).zfill(4) + "_0000.nii.gz")
