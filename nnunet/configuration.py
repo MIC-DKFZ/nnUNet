e57fabc68be4b12b2e21222afd2377b2b412abe5
Marcus Wirtz <marcus.wirtz@rwth-aachen.de>
2021-05-23 15:17:29 +0200

    Update configuration.py

--- nnunet/configuration.py
+++ nnunet/configuration.py
@@ -1,5 +1,8 @@
 import os
+import psutil
 
-default_num_threads = 8 if 'nnUNet_def_n_proc' not in os.environ else int(os.environ['nnUNet_def_n_proc'])
+default_num_threads = max(1, psutil.cpu_count(logical=False) - 1)
+if 'nnUNet_def_n_proc' in os.environ:
+    default_num_threads = int(os.environ['nnUNet_def_n_proc'])
 RESAMPLING_SEPARATE_Z_ANISO_THRESHOLD = 3  # determines what threshold to use for resampling the low resolution axis
 # separately (with NN)
