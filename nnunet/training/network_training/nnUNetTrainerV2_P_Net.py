485169cd42e9ba92a066cc76e04f648620bded01
Karol <KarolGotkowski@gmx.de>
2022-02-07 23:03:27 +0100

    Weird update

--- nnunet/training/network_training/nnUNetTrainerV2_P_Net.py
+++ nnunet/training/network_training/nnUNetTrainerV2_P_Net.py
@@ -35,68 +35,69 @@ import torch
 from nnunet.training.data_augmentation.pyramid_augmentations import MoveSegAsOneHotToData
 
 
-class nnUNetTrainerV2Guided3_P_Net(nnUNetTrainerV2):
+class nnUNetTrainerV2_P_Net(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
         self.max_num_epochs = 1500
         self.initial_lr = 1e-4
+        self.deep_supervision_scales = None
         self.loss = DC_and_CE_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}, {})
 
         self.pin_memory = True
 
-    # def setup_DA_params(self):
-    #     """
-    #     we leave out the creation of self.deep_supervision_scales, so it remains None
-    #     :return:
-    #     """
-    #     if self.threeD:
-    #         self.data_aug_params = default_3D_augmentation_params
-    #         self.data_aug_params['rotation_x'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
-    #         self.data_aug_params['rotation_y'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
-    #         self.data_aug_params['rotation_z'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
-    #         if self.do_dummy_2D_aug:
-    #             self.data_aug_params["dummy_2D"] = True
-    #             self.print_to_log_file("Using dummy2d data augmentation")
-    #             self.data_aug_params["elastic_deform_alpha"] = \
-    #                 default_2D_augmentation_params["elastic_deform_alpha"]
-    #             self.data_aug_params["elastic_deform_sigma"] = \
-    #                 default_2D_augmentation_params["elastic_deform_sigma"]
-    #             self.data_aug_params["rotation_x"] = default_2D_augmentation_params["rotation_x"]
-    #     else:
-    #         self.do_dummy_2D_aug = False
-    #         if max(self.patch_size) / min(self.patch_size) > 1.5:
-    #             default_2D_augmentation_params['rotation_x'] = (-15. / 360 * 2. * np.pi, 15. / 360 * 2. * np.pi)
-    #         self.data_aug_params = default_2D_augmentation_params
-    #     self.data_aug_params["mask_was_used_for_normalization"] = self.use_mask_for_norm
-    #
-    #     if self.do_dummy_2D_aug:
-    #         self.basic_generator_patch_size = get_patch_size(self.patch_size[1:],
-    #                                                          self.data_aug_params['rotation_x'],
-    #                                                          self.data_aug_params['rotation_y'],
-    #                                                          self.data_aug_params['rotation_z'],
-    #                                                          self.data_aug_params['scale_range'])
-    #         self.basic_generator_patch_size = np.array([self.patch_size[0]] + list(self.basic_generator_patch_size))
-    #         patch_size_for_spatialtransform = self.patch_size[1:]
-    #     else:
-    #         self.basic_generator_patch_size = get_patch_size(self.patch_size, self.data_aug_params['rotation_x'],
-    #                                                          self.data_aug_params['rotation_y'],
-    #                                                          self.data_aug_params['rotation_z'],
-    #                                                          self.data_aug_params['scale_range'])
-    #         patch_size_for_spatialtransform = self.patch_size
-    #
-    #     self.data_aug_params["scale_range"] = (0.7, 1.4)
-    #     self.data_aug_params["do_elastic"] = False
-    #     self.data_aug_params['selected_seg_channels'] = [0]
-    #     self.data_aug_params['patch_size_for_spatialtransform'] = patch_size_for_spatialtransform
-    #
-    #     # self.data_aug_params['selected_seg_channels'] = list(range(self.num_classes))  # New
-    #     # self.data_aug_params['selected_seg_channels'] = [0, 1]  # New
-    #     # self.data_aug_params['move_last_seg_chanel_to_data'] = True  # New
-    #     # self.data_aug_params['all_segmentation_labels'] = list(range(1, self.num_classes))  # New
-    #
-    #     self.data_aug_params["num_cached_per_thread"] = 2
+    def setup_DA_params(self):
+        """
+        we leave out the creation of self.deep_supervision_scales, so it remains None
+        :return:
+        """
+        if self.threeD:
+            self.data_aug_params = default_3D_augmentation_params
+            self.data_aug_params['rotation_x'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
+            self.data_aug_params['rotation_y'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
+            self.data_aug_params['rotation_z'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
+            if self.do_dummy_2D_aug:
+                self.data_aug_params["dummy_2D"] = True
+                self.print_to_log_file("Using dummy2d data augmentation")
+                self.data_aug_params["elastic_deform_alpha"] = \
+                    default_2D_augmentation_params["elastic_deform_alpha"]
+                self.data_aug_params["elastic_deform_sigma"] = \
+                    default_2D_augmentation_params["elastic_deform_sigma"]
+                self.data_aug_params["rotation_x"] = default_2D_augmentation_params["rotation_x"]
+        else:
+            self.do_dummy_2D_aug = False
+            if max(self.patch_size) / min(self.patch_size) > 1.5:
+                default_2D_augmentation_params['rotation_x'] = (-15. / 360 * 2. * np.pi, 15. / 360 * 2. * np.pi)
+            self.data_aug_params = default_2D_augmentation_params
+        self.data_aug_params["mask_was_used_for_normalization"] = self.use_mask_for_norm
+
+        if self.do_dummy_2D_aug:
+            self.basic_generator_patch_size = get_patch_size(self.patch_size[1:],
+                                                             self.data_aug_params['rotation_x'],
+                                                             self.data_aug_params['rotation_y'],
+                                                             self.data_aug_params['rotation_z'],
+                                                             self.data_aug_params['scale_range'])
+            self.basic_generator_patch_size = np.array([self.patch_size[0]] + list(self.basic_generator_patch_size))
+            patch_size_for_spatialtransform = self.patch_size[1:]
+        else:
+            self.basic_generator_patch_size = get_patch_size(self.patch_size, self.data_aug_params['rotation_x'],
+                                                             self.data_aug_params['rotation_y'],
+                                                             self.data_aug_params['rotation_z'],
+                                                             self.data_aug_params['scale_range'])
+            patch_size_for_spatialtransform = self.patch_size
+
+        self.data_aug_params["scale_range"] = (0.7, 1.4)
+        self.data_aug_params["do_elastic"] = False
+        self.data_aug_params['selected_seg_channels'] = [0]
+        self.data_aug_params['patch_size_for_spatialtransform'] = patch_size_for_spatialtransform
+
+        # self.data_aug_params['selected_seg_channels'] = list(range(self.num_classes))  # New
+        # self.data_aug_params['selected_seg_channels'] = [0, 1]  # New
+        # self.data_aug_params['move_last_seg_chanel_to_data'] = True  # New
+        # self.data_aug_params['all_segmentation_labels'] = list(range(1, self.num_classes))  # New
+
+        self.data_aug_params["num_cached_per_thread"] = 2
 
     def initialize(self, training=True, force_load_plans=False, mcdo=-1):
         """
@@ -194,5 +195,5 @@ class nnUNetTrainerV2Guided3_P_Net(nnUNetTrainerV2):
             self.network.cuda()
         self.network.inference_apply_nonlin = softmax_helper
 
-    # def run_online_evaluation(self, output, target):
-    #     return nnUNetTrainer.run_online_evaluation(self, output, target)
+    def run_online_evaluation(self, output, target):
+        return nnUNetTrainer.run_online_evaluation(self, output, target)
