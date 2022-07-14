662dc4bb66a5b7c850fb20f148f263bbd4adfa8b
Fabian Isensee <isenseef@gmail.com>
2022-07-14 12:30:52 +0200

    should probably not forget to reset shit to where it should be...

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -117,9 +117,9 @@ class nnUNetTrainer(object):
         self.initial_lr = 1e-2
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
-        self.num_iterations_per_epoch = 100
-        self.num_val_iterations_per_epoch = 10
-        self.num_epochs = 20
+        self.num_iterations_per_epoch = 250
+        self.num_val_iterations_per_epoch = 50
+        self.num_epochs = 1000
         self.current_epoch = 0
 
         ### Dealing with labels/regions
