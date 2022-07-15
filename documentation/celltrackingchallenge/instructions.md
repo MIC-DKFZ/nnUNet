1d12a27f65e81fbf7ce8a8812a03c2af67e449a9
Fabian Isensee <f.isensee@dkfz.de>
2022-07-15 14:40:47 +0200

    ctd with Fluo-N3DH-SIM+

--- documentation/celltrackingchallenge/instructions.md
+++ documentation/celltrackingchallenge/instructions.md
@@ -1,14 +1,14 @@
 # Fluo-C3DH-A549 and Fluo-C3DH-A549-SIM
-
-## Dataset conversion
 These datasets are usable by nnU-Net as is, so all we need to do is convert them into nifti format. 
-Open [Task075_Fluo_C3DH_A549_ManAndSim.py](../../nnunet/dataset_conversion/Task075_Fluo_C3DH_A549_ManAndSim.py), 
-adapt the paths in it and execute it with python.
 
 When participating in the competition we just merged the two datasets into one nnU-Net task and trained for them 
 together. This could have been a good idea or a horrible one. This was not evaluated (time was short and we were young).
 
-Note that this script will create a custom cross-validation split so that we stratify properly. Againg it's not 
+## Dataset conversion
+Open [Task075_Fluo_C3DH_A549_ManAndSim.py](../../nnunet/dataset_conversion/Task075_Fluo_C3DH_A549_ManAndSim.py), 
+adapt the paths in it and execute it with python.
+
+Note that this script will create a custom cross-validation split so that we stratify properly. Again we're not 
 sure whether this is necessary. Just roll with it.
 
 Now run planning and preprocessing with 
@@ -41,11 +41,13 @@ Nah just kidding. We use an age old trick (which we didnt invent) and convert th
 semantic segmentation task. Semantic classes are 'cell center' and 'cell border'. This will allow us to convert back
 to instance segmentation when we are done.
 
-Ah and this dataset is also a time series abd because it may be hard to distinguish cell instances in 2D without time 
+Ah and this dataset is also a time series and because it may be hard to distinguish cell instances in 2D without time 
 information we hack some of that good stuff in there. Essentially we stack the previous images of the frame of interest 
 in the color channel. So the input to the model is [t-4, t-3, t-2, t-1, frame_of_interest]. Stacking in color channels
-is probably not very effective but remember that we are jsut applying nnU-Net here and cannot change the method 
+is probably not very effective but remember that we are just applying nnU-Net here and cannot change the method 
 (it's supposed to be out-of-the-box ;-) ).
+
+## Dataset conversion
 
 Open the file [Task089_Fluo-N2DH-SIM.py](../../nnunet/dataset_conversion/Task089_Fluo-N2DH-SIM.py) and 
 modify the paths. Then execute it with python. This will convert the raw dataset.
@@ -63,4 +65,18 @@ This just trains a single model on all available training cases. No ensembling h
 ## Inference
 You can use the images in `imagesTs` but honestly just use the code we provide for inference 
 [here](http://celltrackingchallenge.net/participants/DKFZ-GE/)
+
+# Fluo-N3DH-SIM+
+Instance segmentation just like Fluo-N2DH-SIM so we convert the instances into a two-call semantic segmentation 
+problem (cell border & center) so that we can solve it with nnU-Net. No messing with time information because 3D is 
+[noice](https://thumbs.gfycat.com/CoordinatedEnergeticChipmunk-size_restricted.gif)! 
+
+## Dataset conversion
+Open the file [Task076_Fluo_N3DH_SIM.py](../../nnunet/dataset_conversion/Task076_Fluo_N3DH_SIM.py) and 
+modify the paths. Then execute it with python. This will convert the raw dataset.
+
+
+## Training
+
+## Inference
 
--- nnunet/dataset_conversion/Task076_Fluo_N3DH_SIM.py
+++ nnunet/dataset_conversion/Task076_Fluo_N3DH_SIM.py
@@ -73,78 +73,6 @@ def find_differences(labelstr1, labelstr2):
         print(n, np.sum(a != b))
 
 
-def prepare_task(base, task_id, task_name, spacing, border_thickness: float = 15, processes: int = 16):
-    p = Pool(processes)
-
-    foldername = "Task%03.0d_%s" % (task_id, task_name)
-
-    out_base = join(nnUNet_raw_data, foldername)
-    imagestr = join(out_base, "imagesTr")
-    imagests = join(out_base, "imagesTs")
-    labelstr = join(out_base, "labelsTr")
-    maybe_mkdir_p(imagestr)
-    maybe_mkdir_p(imagests)
-    maybe_mkdir_p(labelstr)
-
-    train_patient_names = []
-    test_patient_names = []
-    res = []
-
-    for train_sequence in [i for i in subfolders(base + "_train", join=False) if not i.endswith("_GT")]:
-        train_cases = subfiles(join(base + '_train', train_sequence), suffix=".tif", join=False)
-        for t in train_cases:
-            casename = train_sequence + "_" + t[:-4]
-            img_file = join(base + '_train', train_sequence, t)
-            lab_file = join(base + '_train', train_sequence + "_GT", "SEG", "man_seg" + t[1:])
-            if not isfile(lab_file):
-                continue
-            img_out_base = join(imagestr, casename)
-            anno_out = join(labelstr, casename + ".nii.gz")
-            res.append(
-                p.starmap_async(load_bmp_convert_to_nifti_borders, ((img_file, lab_file, img_out_base, anno_out, spacing, border_thickness),)))
-            train_patient_names.append(casename)
-
-    for test_sequence in [i for i in subfolders(base + "_test", join=False) if not i.endswith("_GT")]:
-        test_cases = subfiles(join(base + '_test', test_sequence), suffix=".tif", join=False)
-        for t in test_cases:
-            casename = test_sequence + "_" + t[:-4]
-            img_file = join(base + '_test', test_sequence, t)
-            lab_file = None
-            img_out_base = join(imagests, casename)
-            anno_out = None
-            res.append(
-                p.starmap_async(load_bmp_convert_to_nifti_borders, ((img_file, lab_file, img_out_base, anno_out, spacing, border_thickness),)))
-            test_patient_names.append(casename)
-
-    _ = [i.get() for i in res]
-
-    json_dict = {}
-    json_dict['name'] = task_name
-    json_dict['description'] = ""
-    json_dict['tensorImageSize'] = "4D"
-    json_dict['reference'] = ""
-    json_dict['licence'] = ""
-    json_dict['release'] = "0.0"
-    json_dict['modality'] = {
-        "0": "BF",
-    }
-    json_dict['labels'] = {
-        "0": "background",
-        "1": "cell",
-        "2": "border",
-    }
-
-    json_dict['numTraining'] = len(train_patient_names)
-    json_dict['numTest'] = len(test_patient_names)
-    json_dict['training'] = [{'image': "./imagesTr/%s.nii.gz" % i, "label": "./labelsTr/%s.nii.gz" % i} for i in
-                             train_patient_names]
-    json_dict['test'] = ["./imagesTs/%s.nii.gz" % i for i in test_patient_names]
-
-    save_json(json_dict, os.path.join(out_base, "dataset.json"))
-    p.close()
-    p.join()
-
-
 def plot_images(folder, output_folder):
     maybe_mkdir_p(output_folder)
     import matplotlib.pyplot as plt
@@ -278,15 +206,82 @@ def convert_all_to_instance(input_folder: str, output_folder: str, processes: in
 
 
 if __name__ == "__main__":
-    base = "/home/fabian/data/Fluo-N3DH-SIM"
+    source_train = '/home/isensee/drives/E132-Rohdaten/CellTrackingChallenge/train/Fluo-N3DH-SIM+'
+    source_test = '/home/isensee/drives/E132-Rohdaten/CellTrackingChallenge/test/Fluo-N3DH-SIM+'
+
     task_id = 76
     task_name = 'Fluo_N3DH_SIM'
     spacing = (0.2, 0.125, 0.125)
     border_thickness = 0.5
+    p = Pool(12)
 
-    prepare_task(base, task_id, task_name, spacing, border_thickness, 12)
+    foldername = "Task%03.0d_%s" % (task_id, task_name)
 
-    # we need custom splits
+    out_base = join(nnUNet_raw_data, foldername)
+    imagestr = join(out_base, "imagesTr")
+    imagests = join(out_base, "imagesTs")
+    labelstr = join(out_base, "labelsTr")
+    maybe_mkdir_p(imagestr)
+    maybe_mkdir_p(imagests)
+    maybe_mkdir_p(labelstr)
+
+    train_patient_names = []
+    test_patient_names = []
+    res = []
+
+    for train_sequence in ['01', '02']:
+        train_cases = subfiles(join(source_train, train_sequence), suffix=".tif", join=False)
+        for t in train_cases:
+            casename = train_sequence + "_" + t[:-4]
+            img_file = join(source_train, train_sequence, t)
+            lab_file = join(source_train, train_sequence + "_GT", "SEG", "man_seg" + t[1:])
+            img_out_base = join(imagestr, casename)
+            anno_out = join(labelstr, casename + ".nii.gz")
+            res.append(
+                p.starmap_async(load_bmp_convert_to_nifti_borders, ((img_file, lab_file, img_out_base, anno_out, spacing, border_thickness),)))
+            train_patient_names.append(casename)
+
+    for test_sequence in ['01', '02']:
+        test_cases = subfiles(join(source_test, test_sequence), suffix=".tif", join=False)
+        for t in test_cases:
+            casename = test_sequence + "_" + t[:-4]
+            img_file = join(source_test, test_sequence, t)
+            lab_file = None
+            img_out_base = join(imagests, casename)
+            anno_out = None
+            res.append(
+                p.starmap_async(load_bmp_convert_to_nifti_borders, ((img_file, lab_file, img_out_base, anno_out, spacing, border_thickness),)))
+            test_patient_names.append(casename)
+
+    _ = [i.get() for i in res]
+
+    json_dict = {}
+    json_dict['name'] = task_name
+    json_dict['description'] = ""
+    json_dict['tensorImageSize'] = "4D"
+    json_dict['reference'] = ""
+    json_dict['licence'] = ""
+    json_dict['release'] = "0.0"
+    json_dict['modality'] = {
+        "0": "BF",
+    }
+    json_dict['labels'] = {
+        "0": "background",
+        "1": "cell",
+        "2": "border",
+    }
+
+    json_dict['numTraining'] = len(train_patient_names)
+    json_dict['numTest'] = len(test_patient_names)
+    json_dict['training'] = [{'image': "./imagesTr/%s.nii.gz" % i, "label": "./labelsTr/%s.nii.gz" % i} for i in
+                             train_patient_names]
+    json_dict['test'] = ["./imagesTs/%s.nii.gz" % i for i in test_patient_names]
+
+    save_json(json_dict, os.path.join(out_base, "dataset.json"))
+    p.close()
+    p.join()
+
+    # We dont use this, ignore
     task_name = "Task076_Fluo_N3DH_SIM"
     labelsTr = join(nnUNet_raw_data, task_name, "labelsTr")
     cases = subfiles(labelsTr, suffix='.nii.gz', join=False)
