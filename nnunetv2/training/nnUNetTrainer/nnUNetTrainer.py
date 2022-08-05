277c0ffe99af83449b97696825ee8b7c4b4b4c54
Fabian Isensee <isenseef@gmail.com>
2022-08-05 11:13:14 +0200

    reset to 1000 epochs

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -121,7 +121,7 @@ class nnUNetTrainer(object):
         self.oversample_foreground_percent = 0.33
         self.num_iterations_per_epoch = 250
         self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 10
+        self.num_epochs = 1000
         self.current_epoch = 0
 
         ### Dealing with labels/regions
