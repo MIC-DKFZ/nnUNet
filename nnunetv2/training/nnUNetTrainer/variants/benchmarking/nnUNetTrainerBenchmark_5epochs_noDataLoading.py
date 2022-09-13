a6f7e257b5ba83ffde2b510730fd8bcc99432c31
Fabian Isensee <isenseef@gmail.com>
2022-09-13 10:10:40 +0200

    summarizing benchmarking results

--- nnunetv2/training/nnUNetTrainer/variants/benchmarking/nnUNetTrainerBenchmark_5epochs_noDataLoading.py
+++ nnunetv2/training/nnUNetTrainer/variants/benchmarking/nnUNetTrainerBenchmark_5epochs_noDataLoading.py
@@ -15,31 +15,38 @@ class nnUNetTrainerBenchmark_5epochs_noDataLoading(nnUNetTrainerBenchmark_5epoch
         num_input_channels = determine_num_input_channels(self.plans, self.configuration, self.dataset_json)
         patch_size = self.plans['configurations'][self.configuration]["patch_size"]
         dummy_data = torch.rand((self.batch_size, num_input_channels, *patch_size), device=self.device)
-        dummy_target = [torch.round(torch.rand((self.batch_size, 1, *[int(i * j) for i, j in zip(patch_size, k)]), device=self.device) * max(self.label_manager.all_labels)) for k in self._get_deep_supervision_scales()]
+        dummy_target = [
+            torch.round(
+                torch.rand((self.batch_size, 1, *[int(i * j) for i, j in zip(patch_size, k)]), device=self.device) *
+                max(self.label_manager.all_labels)
+            ) for k in self._get_deep_supervision_scales()]
         self.dummy_batch = {'data': dummy_data, 'target': dummy_target}
 
     def get_dataloaders(self):
         return None, None
 
     def run_training(self):
-        self.on_train_start()
+        try:
+            self.on_train_start()
 
-        for epoch in range(self.current_epoch, self.num_epochs):
-            self.on_epoch_start()
+            for epoch in range(self.current_epoch, self.num_epochs):
+                self.on_epoch_start()
 
-            self.on_train_epoch_start()
-            train_outputs = []
-            for batch_id in range(self.num_iterations_per_epoch):
-                train_outputs.append(self.train_step(self.dummy_batch))
-            self.on_train_epoch_end(train_outputs)
+                self.on_train_epoch_start()
+                train_outputs = []
+                for batch_id in range(self.num_iterations_per_epoch):
+                    train_outputs.append(self.train_step(self.dummy_batch))
+                self.on_train_epoch_end(train_outputs)
 
-            with torch.no_grad():
-                self.on_validation_epoch_start()
-                val_outputs = []
-                for batch_id in range(self.num_val_iterations_per_epoch):
-                    val_outputs.append(self.validation_step(self.dummy_batch))
-                self.on_validation_epoch_end(val_outputs)
+                with torch.no_grad():
+                    self.on_validation_epoch_start()
+                    val_outputs = []
+                    for batch_id in range(self.num_val_iterations_per_epoch):
+                        val_outputs.append(self.validation_step(self.dummy_batch))
+                    self.on_validation_epoch_end(val_outputs)
 
-            self.on_epoch_end()
+                self.on_epoch_end()
 
-        self.on_train_end()
+            self.on_train_end()
+        except RuntimeError:
+            self.crashed_with_runtime_error = True
