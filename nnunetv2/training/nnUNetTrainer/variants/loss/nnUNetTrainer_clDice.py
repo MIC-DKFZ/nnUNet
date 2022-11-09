f2e7b094e9ccecdc46dd46bd227a4333bc09ad96
Fabian Isensee <f.isensee@dkfz.de>
2022-11-09 21:19:25 +0100

    stuff

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_clDice.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_clDice.py
@@ -13,6 +13,30 @@ class nnUNetTrainer_clDice(nnUNetTrainer):
             loss = Dc_BCE_clDice_loss({'batch_dice': self.plans['configurations'][self.configuration]['batch_dice'],
                                        'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=0.5,
                                       weight_dice=0.5, weight_clDice=1, iters=4, smooth_clDice=1e-5,
+                                      ignore_label=self.label_manager.ignore_label, cldice_slicewise=False
+                                      )
+
+        deep_supervision_scales = self._get_deep_supervision_scales()
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+
+        # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+        weights = weights / weights.sum()
+        # now wrap the loss
+        loss = DeepSupervisionWrapper(loss, weights)
+        return loss
+
+
+class nnUNetTrainer_clDice_sliceWise(nnUNetTrainer):
+    def _build_loss(self):
+        if self.label_manager.has_regions:
+            raise NotImplementedError()
+        else:
+            loss = Dc_BCE_clDice_loss({'batch_dice': self.plans['configurations'][self.configuration]['batch_dice'],
+                                       'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=0.5,
+                                      weight_dice=0.5, weight_clDice=1, iters=4, smooth_clDice=1e-5,
                                       ignore_label=self.label_manager.ignore_label, cldice_slicewise=True
                                       )
 
