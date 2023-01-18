fix missing factor 2 in MemoryEfficientSoftDiceLoss

nnunetv2/training/loss/dice.py
@@ -106,7 +106,7 @@ class MemoryEfficientSoftDiceLoss(nn.Module):
             sum_pred = sum_pred.sum(0)
             sum_gt = sum_gt.sum(0)
 
-        dc = (intersect + self.smooth) / (torch.clip(sum_gt + sum_pred + self.smooth, 1e-8))
+        dc = 2 * (intersect + self.smooth) / (torch.clip(sum_gt + sum_pred + self.smooth, 1e-8))
 
         if not self.do_bg:
             if self.batch_dice:
@@ -181,3 +181,15 @@ def get_tp_fp_fn_tn(net_output, gt, axes=None, mask=None, square=False):
         tn = sum_tensor(tn, axes, keepdim=False)
 
     return tp, fp, fn, tn
+
+
+if __name__ == '__main__':
+    from nnunetv2.utilities.helpers import softmax_helper_dim1
+    pred = torch.rand((2, 3, 32, 32, 32))
+    ref = torch.randint(0, 3, (2, 32, 32, 32))
+
+    dl_old = SoftDiceLoss(apply_nonlin=softmax_helper_dim1, batch_dice=True, do_bg=False, smooth=0, ddp=False)
+    dl_new = MemoryEfficientSoftDiceLoss(apply_nonlin=softmax_helper_dim1, batch_dice=True, do_bg=False, smooth=0, ddp=False)
+    res_old = dl_old(pred, ref)
+    res_new = dl_new(pred, ref)
+    print(res_old, res_new)

nnunetv2/training/nnUNetTrainer/variants/sparse_labels/nnUNetTrainer_ignoreLabel_lossBalancing.py
@@ -35,14 +35,14 @@ class DC_and_CE_loss_ignlossbalancing(nn.Module):
                 target_dice = target
             shp_x = net_output.shape
             axes = list(range(2, len(shp_x)))
-            sample_fg_percent = non_ignore_mask.sum((1, *axes)) / np.prod(shp_x[2:])
+            sample_annotated_percent = non_ignore_mask.sum((1, *axes)) / np.prod(shp_x[2:])
         x = softmax_helper_dim1(net_output)
         tp, fp, fn, _ = get_tp_fp_fn_tn(x, target_dice, axes, non_ignore_mask, False)
 
         if self.ddp:
             raise NotImplementedError('No DDP for you!')  # we would need to synchronize sample_weights and ce losses per sample as well
 
-        sample_weights = sample_fg_percent / torch.clip(sample_fg_percent.sum(), min=1e-8) * shp_x[0]
+        sample_weights = sample_annotated_percent / torch.clip(sample_annotated_percent.sum(), min=1e-8) * shp_x[0]
         tp *= sample_weights[:, None]
         fp *= sample_weights[:, None]
         fn *= sample_weights[:, None]