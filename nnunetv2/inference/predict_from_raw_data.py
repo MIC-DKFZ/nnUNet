72278034255bb1ac70aaf5f4aa5fdcac03238d33
Fabian Isensee <f.isensee@dkfz.de>
2023-04-05 16:47:55 +0200

    add assertion

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -168,7 +168,7 @@ def predict_from_raw_data(list_of_lists_or_source_folder: Union[str, List[List[s
                   f'inference of the previous stage...')
             folder_with_segs_from_prev_stage = join(output_folder,
                                                     f'prediction_{configuration_manager.previous_stage_name}')
-            RAISE ASSERTION ERROR!
+            assert num_parts == 1 and part_id == 0
             predict_from_raw_data(list_of_lists_or_source_folder,
                                   folder_with_segs_from_prev_stage,
                                   get_output_folder(plans_manager.dataset_name,
