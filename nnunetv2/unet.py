2561dbfa7c7b22c00625abaddf95dd75d9f8fc70
zhack47 <mesbah.zacharia@sfr.fr>
2023-05-17 13:41:22 +0200

    debug

--- nnunetv2/unet.py
+++ nnunetv2/unet.py
@@ -33,7 +33,7 @@ class UNetEncoderL(nn.Module):
         self.down1 = (Down(32, 64))
         self.down2 = (Down(64, 128))
         self.down3 = (Down(128, 256))
-        self.down4 = (Down(256, 256))
+        self.down4 = (Down(256, 512))
 
     def forward(self, x):
 
