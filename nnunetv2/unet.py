92e8726c9750786ae4eff6c0bcbdec0f42e4434c
zhack47 <mesbah.zacharia@sfr.fr>
2023-05-17 13:50:07 +0200

    debug

--- nnunetv2/unet.py
+++ nnunetv2/unet.py
@@ -30,7 +30,7 @@ class UNetEncoderL(nn.Module):
     def __init__(self, channels):
         super(UNetEncoderL, self).__init__()
         self.inc = (DoubleConv(channels, 32))
-        self.down1 = (Down(32, 64))
+        self.down1 = (Down(32, 64, pooling=(1, 2, 2)))
         self.down2 = (Down(64, 128))
         self.down3 = (Down(128, 256))
         self.down4 = (Down(256, 512))
