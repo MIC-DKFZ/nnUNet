97d45ce65b7994f16769539d6c64025cabce8660
zhack47 <mesbah.zacharia@sfr.fr>
2023-07-21 11:43:33 +0200

    increase it per epoch

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer_autopet.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer_autopet.py
@@ -140,12 +140,12 @@ class nnUNetTrainer_autopet(nnUNetTrainer):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with
-        self.initial_lr = 1e-2
+        self.initial_lr = 1e-4
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
-        self.num_iterations_per_epoch = 250
-        self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 1000
+        self.num_iterations_per_epoch = 1250
+        self.num_val_iterations_per_epoch = 250
+        self.num_epochs = 250
         self.current_epoch = 0
 
         ### Dealing with labels/regions
