a7772a03b45d56f1eb6db6fce44cf340fe02bcdf
zhack47 <mesbah.zacharia@sfr.fr>
2023-07-24 13:35:08 +0200

    lets switch it up (mip in forward)

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer_autopet.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer_autopet.py
@@ -111,24 +111,15 @@ class AutoPETNet(nn.Module):
             return output, classif
         else:
             if isinstance(output, list):
-                print(output[0].shape)
-            else:
-                print(output.shape)
-            if isinstance(output, list):
-
                 for i in range(output[0].shape[0]):
                     print(np.unique((output[0][i] * torch.argmax(classif, dim=1)[i]).cpu().detach().numpy(), return_counts=True))
                     print(torch.argmax(classif, dim=1)[i])
-                    print(classif.shape)
-                    print(type(output))
                     output[0][i] = output[0][i] * torch.argmax(classif, dim=1)[i]
                 return output
             else:
                 for i in range(output.shape[0]):
                     print(np.unique((output[i] * torch.argmax(classif, dim=1)[i]).cpu().detach().numpy(), return_counts=True))
                     print(torch.argmax(classif, dim=1)[i])
-                    print(classif.shape)
-                    print(type(output))
                     output[i] = output[i] * torch.argmax(classif, dim=1)[i]
                 return output
 
