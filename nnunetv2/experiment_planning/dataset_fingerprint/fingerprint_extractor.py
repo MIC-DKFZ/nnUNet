bfe0c1b7e6c83923b10be678d99414d228986318
zhack47 <mesbah.zacharia@sfr.fr>
2023-07-28 12:04:10 +0200

    debug

--- nnunetv2/experiment_planning/dataset_fingerprint/fingerprint_extractor.py
+++ nnunetv2/experiment_planning/dataset_fingerprint/fingerprint_extractor.py
@@ -166,9 +166,7 @@ class DatasetFingerprintExtractor(object):
                                  if 'channel_names' in self.dataset_json.keys()
                                  else self.dataset_json['modality'].keys())
             intensity_statistics_per_channel = {}
-            print(results)
             for i in range(num_channels):
-                print(foreground_intensities_per_channel)
                 intensity_statistics_per_channel[i] = {
                     'mean': float(np.mean(foreground_intensities_per_channel[i])),
                     'median': float(np.median(foreground_intensities_per_channel[i])),
