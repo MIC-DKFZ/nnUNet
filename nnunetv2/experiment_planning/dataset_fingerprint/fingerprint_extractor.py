91e17c8ad5073bc124d92e150ffaf619f5aeb75b
zhack47 <mesbah.zacharia@sfr.fr>
2023-07-28 12:03:11 +0200

    debug

--- nnunetv2/experiment_planning/dataset_fingerprint/fingerprint_extractor.py
+++ nnunetv2/experiment_planning/dataset_fingerprint/fingerprint_extractor.py
@@ -91,6 +91,8 @@ class DatasetFingerprintExtractor(object):
         # Downside is that we need to do this twice (once here and once during preprocessing). Upside is that we don't
         # need to save the cropped data anymore. Given that cropping is not too expensive it makes sense to do it this
         # way. This is only possible because we are now using our new input/output interface.
+        data_cropped, seg_cropped, bbox = crop_to_nonzero(images, segmentation)
+
         foreground_intensities_per_channel, foreground_intensity_stats_per_channel = \
             DatasetFingerprintExtractor.collect_foreground_intensities(seg_cropped, data_cropped,
                                                                        num_samples=num_samples)
