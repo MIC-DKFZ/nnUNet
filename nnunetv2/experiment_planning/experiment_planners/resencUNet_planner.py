d5306f471b800ca243b7687fd06fbe93bf733638
Fabian Isensee <f.isensee@dkfz.de>
2024-02-07 10:22:47 +0100

    fix imports

--- nnunetv2/experiment_planning/experiment_planners/resencUNet_planner.py
+++ nnunetv2/experiment_planning/experiment_planners/resencUNet_planner.py
@@ -1,9 +1,9 @@
 from typing import Union, List, Tuple
 
+from dynamic_network_architectures.architectures.unet import ResidualEncoderUNet
 from torch import nn
 
 from nnunetv2.experiment_planning.experiment_planners.default_experiment_planner import ExperimentPlanner
-from dynamic_network_architectures.architectures.residual_unet import ResidualEncoderUNet
 
 
 class ResEncUNetPlanner(ExperimentPlanner):
