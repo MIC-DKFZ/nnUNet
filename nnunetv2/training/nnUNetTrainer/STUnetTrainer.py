8e737c23f953154e4b6fb8a9ce37c8b7661323a0
zhack47 <mesbah.zacharia@sfr.fr>
2024-03-21 15:36:10 +0100

    HanSeg window

--- nnunetv2/training/nnUNetTrainer/STUnetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/STUnetTrainer.py
@@ -118,7 +118,7 @@ class STUNetTrainer_base_ft(STUNetTrainer_base):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.initial_lr = 1e-4
+        self.initial_lr = 1e-3
         self.num_epochs = 500
 
 
