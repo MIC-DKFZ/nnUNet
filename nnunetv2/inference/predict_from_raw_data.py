cf7696e333b54bcea018dfe44e795cf158dbdfe0
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2024-03-28 22:35:47 +0200

    Using gaussian only when necessary + applied inplace addition

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -573,24 +573,27 @@ class nnUNetPredictor(object):
             predicted_logits = torch.zeros((self.label_manager.num_segmentation_heads, *data.shape[1:]),
                                            dtype=torch.half,
                                            device=results_device)
-            n_predictions = torch.zeros(data.shape[1:], dtype=torch.half, device=results_device)
             if self.use_gaussian:
                 gaussian = compute_gaussian(tuple(self.configuration_manager.patch_size), sigma_scale=1. / 8,
                                             value_scaling_factor=10,
                                             device=results_device)
+                n_predictions = torch.zeros(data.shape[1:], dtype=torch.half, device=results_device)
 
-            if self.verbose: print('running prediction')
-            if not self.allow_tqdm and self.verbose: print(f'{len(slicers)} steps')
+            if not self.allow_tqdm and self.verbose:
+                print(f'running prediction: {len(slicers)} steps')
             for sl in tqdm(slicers, disable=not self.allow_tqdm):
                 workon = data[sl][None]
-                workon = workon.to(self.device, non_blocking=False)
+                workon = workon.to(self.device)
 
                 prediction = self._internal_maybe_mirror_and_predict(workon)[0].to(results_device)
 
-                predicted_logits[sl] += (prediction * gaussian if self.use_gaussian else prediction)
-                n_predictions[sl[1:]] += (gaussian if self.use_gaussian else 1)
+                if self.use_gaussian:
+                    prediction *= gaussian
+                    n_predictions[sl[1:]] += gaussian
+                predicted_logits[sl] += prediction
 
-            predicted_logits /= n_predictions
+            if self.use_gaussian:
+                predicted_logits /= n_predictions
             # check for infs
             if torch.any(torch.isinf(predicted_logits)):
                 raise RuntimeError('Encountered inf in predicted array. Aborting... If this problem persists, '
@@ -648,7 +651,7 @@ class nnUNetPredictor(object):
 
             empty_cache(self.device)
             # revert padding
-            predicted_logits = predicted_logits[tuple([slice(None), *slicer_revert_padding[1:]])]
+            predicted_logits = predicted_logits[(slice(None), *slicer_revert_padding[1:])]
         return predicted_logits
 
 
