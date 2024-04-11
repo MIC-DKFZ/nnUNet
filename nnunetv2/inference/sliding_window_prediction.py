22945aa56bb6d3f36a1a6cae4ed3408e6c9f49d7
Fabian Isensee <f.isensee@dkfz.de>
2024-04-11 15:18:57 +0200

    move down the line transferring the gaussian_importance_map to device as this may cause results to be different

--- nnunetv2/inference/sliding_window_prediction.py
+++ nnunetv2/inference/sliding_window_prediction.py
@@ -18,9 +18,9 @@ def compute_gaussian(tile_size: Union[Tuple[int, ...], List[int]], sigma_scale: 
     gaussian_importance_map = gaussian_filter(tmp, sigmas, 0, mode='constant', cval=0)
 
     gaussian_importance_map = torch.from_numpy(gaussian_importance_map)
-    gaussian_importance_map = gaussian_importance_map.to(device=device, dtype=dtype)
 
     gaussian_importance_map /= (torch.max(gaussian_importance_map) / value_scaling_factor)
+    gaussian_importance_map = gaussian_importance_map.to(device=device, dtype=dtype)
     # gaussian_importance_map cannot be 0, otherwise we may end up with nans!
     mask = gaussian_importance_map == 0
     gaussian_importance_map[mask] = torch.min(gaussian_importance_map[~mask])
