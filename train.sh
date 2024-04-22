2c97db3cb42b09a569a3b78dc6ea2308d4b6a062
Patrick Strasser <patrick.strasser@stud-mail.uni-wuerzburg.de>
2024-04-22 19:13:55 +0000

    feat: move train.sh to scripts folder

--- train.sh
+++ train.sh
@@ -3,7 +3,8 @@ source ~/env_file
 
 eval "$(conda shell.bash hook)"
 conda activate generic-nnunet
-git checkout dev
+
+cd /home/stud/strasser/workspace/nnUNet && git checkout dev
 
 echo "starting training with fold $fold"
-nnUNetv2_train 137 3d_fullres  $fold --npz --c
+nnUNetv2_train 42 3d_fullres  $fold --npz
