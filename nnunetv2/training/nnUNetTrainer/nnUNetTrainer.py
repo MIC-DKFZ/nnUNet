72b99b74b424320050c1f452eb2d652f96dab77b
Aniruddha <aniruddha0203@gmail.com>
2024-07-24 22:11:52 +0000

    Modify dataloader output to re-use train and val step functions

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -1344,104 +1344,6 @@ class nnUNetTrainerPyTorchDataloader(nnUNetTrainer):
                                     num_images_properties_loading_threshold=0)
         
         return dataset_tr, dataset_val
-
-    # Train and Val step need to be reimplemented for first 2 lines
-    # original nnUnet dataloader returns dict
-    # Pytorch data loader returns tuple
-    def train_step(self, batch: dict) -> dict:
-        data = batch[0]
-        target = batch[1]
-
-        data = data.to(self.device, non_blocking=True)
-        if isinstance(target, list):
-            target = [i.to(self.device, non_blocking=True) for i in target]
-        else:
-            target = target.to(self.device, non_blocking=True)
-
-        self.optimizer.zero_grad(set_to_none=True)
-        # Autocast is a little bitch.
-        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
-        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
-        # So autocast will only be active if we have a cuda device.
-        with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
-            output = self.network(data)
-            # del data
-            l = self.loss(output, target)
-
-        if self.grad_scaler is not None:
-            self.grad_scaler.scale(l).backward()
-            self.grad_scaler.unscale_(self.optimizer)
-            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
-            self.grad_scaler.step(self.optimizer)
-            self.grad_scaler.update()
-        else:
-            l.backward()
-            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
-            self.optimizer.step()
-        return {'loss': l.detach().cpu().numpy()}        
-    
-    def validation_step(self, batch: dict) -> dict:
-        data = batch[0]
-        target = batch[1]
-
-        data = data.to(self.device, non_blocking=True)
-        if isinstance(target, list):
-            target = [i.to(self.device, non_blocking=True) for i in target]
-        else:
-            target = target.to(self.device, non_blocking=True)
-
-        # Autocast is a little bitch.
-        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
-        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
-        # So autocast will only be active if we have a cuda device.
-        with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
-            output = self.network(data)
-            del data
-            l = self.loss(output, target)
-
-        # we only need the output with the highest output resolution
-        output = output[0]
-        target = target[0]
-
-        # the following is needed for online evaluation. Fake dice (green line)
-        axes = [0] + list(range(2, output.ndim))
-
-        if self.label_manager.has_regions:
-            predicted_segmentation_onehot = (torch.sigmoid(output) > 0.5).long()
-        else:
-            # no need for softmax
-            output_seg = output.argmax(1)[:, None]
-            predicted_segmentation_onehot = torch.zeros(output.shape, device=output.device, dtype=torch.float32)
-            predicted_segmentation_onehot.scatter_(1, output_seg, 1)
-            del output_seg
-
-        if self.label_manager.has_ignore_label:
-            if not self.label_manager.has_regions:
-                mask = (target != self.label_manager.ignore_label).float()
-                # CAREFUL that you don't rely on target after this line!
-                target[target == self.label_manager.ignore_label] = 0
-            else:
-                mask = 1 - target[:, -1:]
-                # CAREFUL that you don't rely on target after this line!
-                target = target[:, :-1]
-        else:
-            mask = None
-
-        tp, fp, fn, _ = get_tp_fp_fn_tn(predicted_segmentation_onehot, target, axes=axes, mask=mask)
-
-        tp_hard = tp.detach().cpu().numpy()
-        fp_hard = fp.detach().cpu().numpy()
-        fn_hard = fn.detach().cpu().numpy()
-        if not self.label_manager.has_regions:
-            # if we train with regions all segmentation heads predict some kind of foreground. In conventional
-            # (softmax training) there needs tobe one output for the background. We are not interested in the
-            # background Dice
-            # [1:] in order to remove background
-            tp_hard = tp_hard[1:]
-            fp_hard = fp_hard[1:]
-            fn_hard = fn_hard[1:]
-
-        return {'loss': l.detach().cpu().numpy(), 'tp_hard': tp_hard, 'fp_hard': fp_hard, 'fn_hard': fn_hard}    
     
     def run_training(self):
         self.on_train_start()
@@ -1452,7 +1354,9 @@ class nnUNetTrainerPyTorchDataloader(nnUNetTrainer):
             self.on_train_epoch_start()
             train_outputs = []
             for batch_id in range(self.num_iterations_per_epoch):
-                train_outputs.append(self.train_step(next(iter(self.dataloader_train))))
+                train_batch_tuple = next(iter(self.dataloader_train))                
+                train_batch = {"data": train_batch_tuple[0], "target": train_batch_tuple[1]}
+                train_outputs.append(self.train_step(train_batch))
             self.on_train_epoch_end(train_outputs)
 
             with torch.no_grad():
@@ -1460,7 +1364,9 @@ class nnUNetTrainerPyTorchDataloader(nnUNetTrainer):
                 val_outputs = []
                 # Rewritten because PyTorch dataloader accessed using next(iter())
                 for batch_id in range(self.num_val_iterations_per_epoch):
-                    val_outputs.append(self.validation_step(next(iter(self.dataloader_val))))
+                    val_batch_tuple = next(iter(self.dataloader_val))
+                    val_batch = {"data": val_batch_tuple[0], "target": val_batch_tuple[1]}
+                    val_outputs.append(self.validation_step(val_batch))
                 self.on_validation_epoch_end(val_outputs)
 
             self.on_epoch_end()
