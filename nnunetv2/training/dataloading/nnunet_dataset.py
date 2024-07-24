b7c134040fb1b08b2b8576872877b011ffa14680
Aniruddha <aniruddha0203@gmail.com>
2024-07-24 00:58:11 +0000

    syntax error inaccessing function

--- nnunetv2/training/dataloading/nnunet_dataset.py
+++ nnunetv2/training/dataloading/nnunet_dataset.py
@@ -153,7 +153,7 @@ class nnUNetPytorchDataset(Dataset, nnUNetDataset):
     def load_case(self, idx):
         # This is different because Pytorch indexes with numbers not keys
         key = list(self.keys())[idx]
-        entry = self._get_case_dict[key]
+        entry = self._get_case_dict(key)
         if 'open_data_file' in entry.keys():
             data = entry['open_data_file']
             # print('using open data file')
