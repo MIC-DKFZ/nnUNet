575e129e84d9e2b50d7f6dd74e1c72632f277cb8
Maximilian Rokuss <maximilian.rokuss@dkfz-heidelberg.de>
2024-10-15 18:49:38 +0200

    aortaseg24

---
+++ nnunetv2/training/nnUNetTrainer/variants/competitions/aortaseg24.py
@@ -0,0 +1,5 @@
+from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerNoMirroring import nnUNetTrainer_onlyMirror01
+from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerDA5 import nnUNetTrainerDA5
+
+class nnUNetTrainer_onlyMirror01_DA5(nnUNetTrainer_onlyMirror01, nnUNetTrainerDA5):
+    pass
