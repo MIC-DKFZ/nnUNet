ad3c9bd7ce396c18154433ed7a95602e7c412cab
Fabian Isensee <f.isensee@dkfz.de>
2025-06-01 15:01:10 +0200

    n proc DA

--- nnunetv2/utilities/default_n_proc_DA.py
+++ nnunetv2/utilities/default_n_proc_DA.py
@@ -21,7 +21,7 @@ def get_allowed_n_proc_DA():
         use_this = int(os.environ['nnUNet_n_proc_DA'])
     else:
         hostname = subprocess.getoutput(['hostname'])
-        if hostname in ['Fabian', ]:
+        if hostname in ['Fabian', 'isensee-']:
             use_this = 12
         elif hostname in ['hdf19-gpu16', 'hdf19-gpu17', 'hdf19-gpu18', 'hdf19-gpu19', 'e230-AMDworkstation']:
             use_this = 16
@@ -33,6 +33,8 @@ def get_allowed_n_proc_DA():
             use_this = 6
         elif hostname.startswith('e230-dgxa100-'):
             use_this = 28
+        elif hostname.startswith('e230-thinka100-'):
+            use_this = 20
         elif hostname.startswith('lsf22-gpu'):
             use_this = 28
         elif hostname.startswith('hdf19-gpu') or hostname.startswith('e071-gpu'):
