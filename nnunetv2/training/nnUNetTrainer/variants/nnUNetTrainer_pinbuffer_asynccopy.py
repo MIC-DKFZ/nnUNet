364afb502d08e8f103fe63eb108499f24a5c94b0
Fabian Isensee <f.isensee@dkfz.de>
2025-12-16 14:39:52 +0100

    nnUNetTrainer_pinbuffer_asynccopy_noDA

--- nnunetv2/training/nnUNetTrainer/variants/nnUNetTrainer_pinbuffer_asynccopy.py
+++ nnunetv2/training/nnUNetTrainer/variants/nnUNetTrainer_pinbuffer_asynccopy.py
@@ -1,10 +1,17 @@
+from typing import Union, Tuple, List
+
+import numpy as np
 import torch
 from batchgenerators.dataloading.single_threaded_augmenter import SingleThreadedAugmenter
 from batchgenerators.dataloading.pinned_buffer_nondet_mta import PinnedBufferNonDetMultiThreadedAugmenter
+from batchgeneratorsv2.helpers.scalar_type import RandomScalar
+from batchgeneratorsv2.transforms.base.basic_transform import BasicTransform
+
 from nnunetv2.training.dataloading.data_loader import nnUNetDataLoader
 from nnunetv2.training.dataloading.nnunet_dataset import infer_dataset_class
 from nnunetv2.training.loss.dice import get_tp_fp_fn_tn
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerNoDA import nnUNetTrainerNoDA
 from nnunetv2.utilities.default_n_proc_DA import get_allowed_n_proc_DA
 from nnunetv2.utilities.helpers import dummy_context
 from torch import autocast
@@ -304,3 +311,30 @@ class nnUNetTrainer_pinbuffer_asynccopy(nnUNetTrainer):
         batch, handle = next(mt_gen_val)
         mt_gen_val.release(handle)
         return mt_gen_train, mt_gen_val
+
+
+class nnUNetTrainer_pinbuffer_asynccopy_noDA(nnUNetTrainer_pinbuffer_asynccopy):
+    @staticmethod
+    def get_training_transforms(
+            patch_size: Union[np.ndarray, Tuple[int]],
+            rotation_for_DA: RandomScalar,
+            deep_supervision_scales: Union[List, Tuple, None],
+            mirror_axes: Tuple[int, ...],
+            do_dummy_2d_data_aug: bool,
+            use_mask_for_norm: List[bool] = None,
+            is_cascaded: bool = False,
+            foreground_labels: Union[Tuple[int, ...], List[int]] = None,
+            regions: List[Union[List[int], Tuple[int, ...], int]] = None,
+            ignore_label: int = None,
+    ) -> BasicTransform:
+        return nnUNetTrainer.get_validation_transforms(deep_supervision_scales, is_cascaded, foreground_labels,
+                                                       regions, ignore_label)
+
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        # we need to disable mirroring here so that no mirroring will be applied in inference!
+        rotation_for_DA, do_dummy_2d_data_aug, _, _ = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        initial_patch_size = self.configuration_manager.patch_size
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
