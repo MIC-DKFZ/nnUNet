1bfa757c39a2ee40f9faac63b01d16e25dca9cdb
Fabian Isensee <f.isensee@dkfz.de>
2026-02-05 14:04:16 +0100

    use no_sync in validation

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -1262,142 +1262,135 @@ class nnUNetTrainer(object):
                                         self.dataset_json, self.__class__.__name__,
                                         self.inference_allowed_mirroring_axes)
 
-        with multiprocessing.get_context("spawn").Pool(default_num_processes) as segmentation_export_pool:
-            worker_list = [i for i in segmentation_export_pool._pool]
-            validation_output_folder = join(self.output_folder, 'validation')
-            maybe_mkdir_p(validation_output_folder)
+        with DDP.no_sync() if self.is_ddp else dummy_context():
+            with multiprocessing.get_context("spawn").Pool(default_num_processes) as segmentation_export_pool:
+                worker_list = [i for i in segmentation_export_pool._pool]
+                validation_output_folder = join(self.output_folder, 'validation')
+                maybe_mkdir_p(validation_output_folder)
 
-            # we cannot use self.get_tr_and_val_datasets() here because we might be DDP and then we have to distribute
-            # the validation keys across the workers.
-            _, val_keys = self.do_split()
-            if self.is_ddp:
-                last_barrier_at_idx = len(val_keys) // self.world_size - 1
+                # we cannot use self.get_tr_and_val_datasets() here because we might be DDP and then we have to distribute
+                # the validation keys across the workers.
+                _, val_keys = self.do_split()
+                if self.is_ddp:
+                    val_keys = val_keys[self.global_rank:: self.world_size]
 
-                val_keys = val_keys[self.global_rank:: self.world_size]
-                # we cannot just have barriers all over the place because the number of keys each GPU receives can be
-                # different
+                dataset_val = self.dataset_class(self.preprocessed_dataset_folder, val_keys,
+                                                 folder_with_segs_from_previous_stage=self.folder_with_segs_from_previous_stage)
+                next_stages = self.configuration_manager.next_stage_names
 
-            dataset_val = self.dataset_class(self.preprocessed_dataset_folder, val_keys,
-                                             folder_with_segs_from_previous_stage=self.folder_with_segs_from_previous_stage)
+                if next_stages is not None:
+                    _ = [maybe_mkdir_p(join(self.output_folder_base, 'predicted_next_stage', n)) for n in next_stages]
 
-            next_stages = self.configuration_manager.next_stage_names
+                results = []
 
-            if next_stages is not None:
-                _ = [maybe_mkdir_p(join(self.output_folder_base, 'predicted_next_stage', n)) for n in next_stages]
-
-            results = []
-
-            for i, k in enumerate(dataset_val.identifiers):
-                proceed = not check_workers_alive_and_busy(segmentation_export_pool, worker_list, results,
-                                                           allowed_num_queued=2)
-                while not proceed:
-                    sleep(0.1)
+                for i, k in enumerate(dataset_val.identifiers):
                     proceed = not check_workers_alive_and_busy(segmentation_export_pool, worker_list, results,
                                                                allowed_num_queued=2)
+                    while not proceed:
+                        sleep(0.1)
+                        proceed = not check_workers_alive_and_busy(segmentation_export_pool, worker_list, results,
+                                                                   allowed_num_queued=2)
 
-                self.print_to_log_file(f"predicting {k}")
-                data, _, seg_prev, properties = dataset_val.load_case(k)
+                    self.print_to_log_file(f"predicting {k}")
+                    data, _, seg_prev, properties = dataset_val.load_case(k)
 
-                # we do [:] to convert blosc2 to numpy
-                data = data[:]
+                    # we do [:] to convert blosc2 to numpy
+                    data = data[:]
 
-                if self.is_cascaded:
-                    seg_prev = seg_prev[:]
-                    data = np.vstack((data, convert_labelmap_to_one_hot(seg_prev, self.label_manager.foreground_labels,
-                                                                        output_dtype=data.dtype)))
-                with warnings.catch_warnings():
-                    # ignore 'The given NumPy array is not writable' warning
-                    warnings.simplefilter("ignore")
-                    data = torch.from_numpy(data)
+                    if self.is_cascaded:
+                        seg_prev = seg_prev[:]
+                        data = np.vstack((data, convert_labelmap_to_one_hot(seg_prev, self.label_manager.foreground_labels,
+                                                                            output_dtype=data.dtype)))
+                    with warnings.catch_warnings():
+                        # ignore 'The given NumPy array is not writable' warning
+                        warnings.simplefilter("ignore")
+                        data = torch.from_numpy(data)
 
-                self.print_to_log_file(f'{k}, shape {data.shape}, global rank {self.global_rank}')
-                output_filename_truncated = join(validation_output_folder, k)
+                    self.print_to_log_file(f'{k}, shape {data.shape}, global rank {self.global_rank}')
+                    output_filename_truncated = join(validation_output_folder, k)
 
-                prediction = predictor.predict_sliding_window_return_logits(data)
-                prediction = prediction.cpu()
+                    prediction = predictor.predict_sliding_window_return_logits(data)
+                    prediction = prediction.cpu()
 
-                # this needs to go into background processes
-                results.append(
-                    segmentation_export_pool.starmap_async(
-                        export_prediction_from_logits, (
-                            (prediction, properties, self.configuration_manager, self.plans_manager,
-                             self.dataset_json, output_filename_truncated, save_probabilities),
+                    # this needs to go into background processes
+                    results.append(
+                        segmentation_export_pool.starmap_async(
+                            export_prediction_from_logits, (
+                                (prediction, properties, self.configuration_manager, self.plans_manager,
+                                 self.dataset_json, output_filename_truncated, save_probabilities),
+                            )
                         )
                     )
-                )
-                # for debug purposes
-                # export_prediction_from_logits(prediction, properties, self.configuration_manager, self.plans_manager,
-                #      self.dataset_json, output_filename_truncated, save_probabilities)
+                    # for debug purposes
+                    # export_prediction_from_logits(prediction, properties, self.configuration_manager, self.plans_manager,
+                    #      self.dataset_json, output_filename_truncated, save_probabilities)
 
-                # if needed, export the softmax prediction for the next stage
-                if next_stages is not None:
-                    for n in next_stages:
-                        next_stage_config_manager = self.plans_manager.get_configuration(n)
-                        expected_preprocessed_folder = join(nnUNet_preprocessed, self.plans_manager.dataset_name,
-                                                            next_stage_config_manager.data_identifier)
-                        # next stage may have a different dataset class, do not use self.dataset_class
-                        dataset_class = infer_dataset_class(expected_preprocessed_folder)
+                    # if needed, export the softmax prediction for the next stage
+                    if next_stages is not None:
+                        for n in next_stages:
+                            next_stage_config_manager = self.plans_manager.get_configuration(n)
+                            expected_preprocessed_folder = join(nnUNet_preprocessed, self.plans_manager.dataset_name,
+                                                                next_stage_config_manager.data_identifier)
+                            # next stage may have a different dataset class, do not use self.dataset_class
+                            dataset_class = infer_dataset_class(expected_preprocessed_folder)
 
-                        try:
-                            # we do this so that we can use load_case and do not have to hard code how loading training cases is implemented
-                            tmp = dataset_class(expected_preprocessed_folder, [k])
-                            d, _, _, _ = tmp.load_case(k)
-                        except FileNotFoundError:
-                            self.print_to_log_file(
-                                f"Predicting next stage {n} failed for case {k} because the preprocessed file is missing! "
-                                f"Run the preprocessing for this configuration first!")
-                            continue
+                            try:
+                                # we do this so that we can use load_case and do not have to hard code how loading training cases is implemented
+                                tmp = dataset_class(expected_preprocessed_folder, [k])
+                                d, _, _, _ = tmp.load_case(k)
+                            except FileNotFoundError:
+                                self.print_to_log_file(
+                                    f"Predicting next stage {n} failed for case {k} because the preprocessed file is missing! "
+                                    f"Run the preprocessing for this configuration first!")
+                                continue
 
-                        target_shape = d.shape[1:]
-                        output_folder = join(self.output_folder_base, 'predicted_next_stage', n)
-                        output_file_truncated = join(output_folder, k)
+                            target_shape = d.shape[1:]
+                            output_folder = join(self.output_folder_base, 'predicted_next_stage', n)
+                            output_file_truncated = join(output_folder, k)
 
-                        # resample_and_save(prediction, target_shape, output_file_truncated, self.plans_manager,
-                        #          self.configuration_manager,
-                        #          properties,
-                        #          self.dataset_json,
-                        #          default_num_processes,
-                        #          dataset_class)
-                        results.append(segmentation_export_pool.starmap_async(
-                            resample_and_save, (
-                                (prediction, target_shape, output_file_truncated, self.plans_manager,
-                                 self.configuration_manager,
-                                 properties,
-                                 self.dataset_json,
-                                 default_num_processes,
-                                 dataset_class),
-                            )
-                        ))
-                # if we don't barrier from time to time we will get nccl timeouts for large datasets. Yuck.
-                if self.is_ddp and i < last_barrier_at_idx and (i + 1) % 20 == 0:
-                    dist.barrier()
+                            # resample_and_save(prediction, target_shape, output_file_truncated, self.plans_manager,
+                            #          self.configuration_manager,
+                            #          properties,
+                            #          self.dataset_json,
+                            #          default_num_processes,
+                            #          dataset_class)
+                            results.append(segmentation_export_pool.starmap_async(
+                                resample_and_save, (
+                                    (prediction, target_shape, output_file_truncated, self.plans_manager,
+                                     self.configuration_manager,
+                                     properties,
+                                     self.dataset_json,
+                                     default_num_processes,
+                                     dataset_class),
+                                )
+                            ))
 
-            _ = [r.get() for r in results]
+                _ = [r.get() for r in results]
+
+            if self.global_rank == 0:
+                # running metric computation only on rank 0, but using more processes if we are distributed and have more
+                # than one local GPU. Unfortunately all other ranks will have to wait for this. Should not take too long
+                # in practice though.
+                # We also switch this to number of DA workers rather than default_num_processes
+                local_world_size = torch.cuda.device_count() if self.is_ddp and torch.cuda.is_available() else 1
+                metrics = compute_metrics_on_folder(join(self.preprocessed_dataset_folder_base, 'gt_segmentations'),
+                                                    validation_output_folder,
+                                                    join(validation_output_folder, 'summary.json'),
+                                                    self.plans_manager.image_reader_writer_class(),
+                                                    self.dataset_json["file_ending"],
+                                                    self.label_manager.foreground_regions if self.label_manager.has_regions else
+                                                    self.label_manager.foreground_labels,
+                                                    self.label_manager.ignore_label, chill=True,
+                                                    num_processes=get_allowed_n_proc_DA() * local_world_size)
+                self.print_to_log_file("Validation complete", also_print_to_console=True)
+                self.print_to_log_file("Mean Validation Dice: ", (metrics['foreground_mean']["Dice"]),
+                                       also_print_to_console=True)
+
+            self.set_deep_supervision_enabled(True)
+            compute_gaussian.cache_clear()
 
         if self.is_ddp:
             dist.barrier()
-
-        if self.global_rank == 0:
-            # running metric computation only on rank 0, but using more processes if we are distributed and have more
-            # than one local GPU. Unfortunately all other ranks will have to wait for this. Should not take too long
-            # in practice though.
-            # We also switch this to number of DA workers rather than default_num_processes
-            local_world_size = torch.cuda.device_count() if self.is_ddp and torch.cuda.is_available() else 1
-            metrics = compute_metrics_on_folder(join(self.preprocessed_dataset_folder_base, 'gt_segmentations'),
-                                                validation_output_folder,
-                                                join(validation_output_folder, 'summary.json'),
-                                                self.plans_manager.image_reader_writer_class(),
-                                                self.dataset_json["file_ending"],
-                                                self.label_manager.foreground_regions if self.label_manager.has_regions else
-                                                self.label_manager.foreground_labels,
-                                                self.label_manager.ignore_label, chill=True,
-                                                num_processes=get_allowed_n_proc_DA() * local_world_size)
-            self.print_to_log_file("Validation complete", also_print_to_console=True)
-            self.print_to_log_file("Mean Validation Dice: ", (metrics['foreground_mean']["Dice"]),
-                                   also_print_to_console=True)
-
-        self.set_deep_supervision_enabled(True)
-        compute_gaussian.cache_clear()
 
     def run_training(self):
         self.on_train_start()
