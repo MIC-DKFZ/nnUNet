0bdb4de4ec3a20dd5426d369c46e0fc08f2760da
entity079 <153630586+entity079@users.noreply.github.com>
2026-02-24 00:35:39 +0530

    Update nnUNetTrainer.py

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -144,7 +144,7 @@ class nnUNetTrainer(object):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with
-        self.initial_lr = 1e-3
+        self.initial_lr = 1e-2
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
         self.probabilistic_oversampling = False
