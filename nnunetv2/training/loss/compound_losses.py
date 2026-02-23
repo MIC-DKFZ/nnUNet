897f38be07fc4290edd4a662f2c1ebef6df197b0
entity079 <153630586+entity079@users.noreply.github.com>
2026-02-24 00:04:55 +0530

    Update compound_losses.py

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -1,64 +1,80 @@
 import torch
 from nnunetv2.training.loss.dice import SoftDiceLoss, MemoryEfficientSoftDiceLoss
+from nnunetv2.training.loss.fpr_loss import SoftFPRLoss, MemoryEfficientSoftFPRLoss
 from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss, TopKLoss
 from nnunetv2.utilities.helpers import softmax_helper_dim1
 from torch import nn
+import logging
 
+class DC_CE_FNR_loss(nn.Module):
+    def __init__(self,
+                 soft_dice_kwargs,
+                 ce_kwargs,
+                 soft_fpr_kwargs=None,
+                 weight_ce=1,
+                 weight_dice=1,
+                 weight_fpr=1,
+                 ignore_label=None,
+                 dice_class=SoftDiceLoss,
+                 fpr_class=SoftFPRLoss):
 
-class DC_and_CE_loss(nn.Module):
-    def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce=1, weight_dice=1, ignore_label=None,
-                 dice_class=SoftDiceLoss):
-        """
-        Weights for CE and Dice do not need to sum to one. You can set whatever you want.
-        :param soft_dice_kwargs:
-        :param ce_kwargs:
-        :param aggregate:
-        :param square_dice:
-        :param weight_ce:
-        :param weight_dice:
-        """
-        super(DC_and_CE_loss, self).__init__()
+        super().__init__()
+
         if ignore_label is not None:
             ce_kwargs['ignore_index'] = ignore_label
 
         self.weight_dice = weight_dice
         self.weight_ce = weight_ce
+        self.weight_fpr = weight_fpr
         self.ignore_label = ignore_label
+        soft_fpr_kwargs = soft_fpr_kwargs or {}
 
         self.ce = RobustCrossEntropyLoss(**ce_kwargs)
         self.dc = dice_class(apply_nonlin=softmax_helper_dim1, **soft_dice_kwargs)
 
+        # Only create FPR if weight > 0
+        if weight_fpr > 0:
+            self.fpr = fpr_class(apply_nonlin=softmax_helper_dim1, **soft_fpr_kwargs)
+        else:
+            self.fpr = None
     def forward(self, net_output: torch.Tensor, target: torch.Tensor):
-        """
-        target must be b, c, x, y(, z) with c=1
-        :param net_output:
-        :param target:
-        :return:
-        """
+
         if self.ignore_label is not None:
-            assert target.shape[1] == 1, 'ignore label is not implemented for one hot encoded target variables ' \
-                                         '(DC_and_CE_loss)'
+            assert target.shape[1] == 1
             mask = target != self.ignore_label
-            # remove ignore label from target, replace with one of the known labels. It doesn't matter because we
-            # ignore gradients in those areas anyway
-            target_dice = torch.where(mask, target, 0)
+            target_clean = torch.where(mask, target, 0)
             num_fg = mask.sum()
         else:
-            target_dice = target
+            target_clean = target
             mask = None
-
-        dc_loss = self.dc(net_output, target_dice, loss_mask=mask) \
+    
+        # Dice
+        dc_loss = self.dc(net_output, target_clean, loss_mask=mask) \
             if self.weight_dice != 0 else 0
+    
+        # CE
         ce_loss = self.ce(net_output, target[:, 0]) \
             if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
-
-        result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
+    
+        # FPR
+        if self.weight_fpr != 0:
+            fpr_loss = self.fpr(net_output, target_clean, loss_mask=mask)
+        else:
+            fpr_loss = 0
+    
+        result = (
+            self.weight_ce * ce_loss +
+            self.weight_dice * dc_loss +
+            self.weight_fpr * fpr_loss
+        )
+    
+        logging.info(f"CE: {ce_loss.item():.4f}, Dice: {dc_loss.item():.4f}, FPR: {fpr_loss.item():.4f}")
+    
         return result
 
-
 class DC_and_BCE_loss(nn.Module):
-    def __init__(self, bce_kwargs, soft_dice_kwargs, weight_ce=1, weight_dice=1, use_ignore_label: bool = False,
-                 dice_class=MemoryEfficientSoftDiceLoss):
+    def __init__(self, bce_kwargs, soft_dice_kwargs, weight_ce=1, weight_dice=1,weight_fpr=1, use_ignore_label: bool = False,
+                 dice_class=MemoryEfficientSoftDiceLoss,soft_fpr_kwargs=None,fpr_class=MemoryEfficientSoftFPRLoss):
         """
         DO NOT APPLY NONLINEARITY IN YOUR NETWORK!
 
@@ -70,15 +86,24 @@ class DC_and_BCE_loss(nn.Module):
         :param aggregate:
         """
         super(DC_and_BCE_loss, self).__init__()
+        
         if use_ignore_label:
             bce_kwargs['reduction'] = 'none'
 
         self.weight_dice = weight_dice
         self.weight_ce = weight_ce
         self.use_ignore_label = use_ignore_label
+        soft_fpr_kwargs = soft_fpr_kwargs or {}
 
         self.ce = nn.BCEWithLogitsLoss(**bce_kwargs)
         self.dc = dice_class(apply_nonlin=torch.sigmoid, **soft_dice_kwargs)
+        self.weight_fpr = weight_fpr
+
+        if weight_fpr > 0:
+            self.fpr = fpr_class(apply_nonlin=torch.sigmoid, **soft_fpr_kwargs)
+        else:
+            self.fpr = None
+
 
     def forward(self, net_output: torch.Tensor, target: torch.Tensor):
         if self.use_ignore_label:
@@ -101,56 +126,77 @@ class DC_and_BCE_loss(nn.Module):
             ce_loss = (self.ce(net_output, target_regions) * mask).sum() / torch.clip(mask.sum(), min=1e-8)
         else:
             ce_loss = self.ce(net_output, target_regions)
-        result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
+        fpr_loss = self.fpr(net_output, target_regions, loss_mask=mask) \
+        if self.weight_fpr != 0 else 0
+    
+        result = (
+            self.weight_ce * ce_loss +
+            self.weight_dice * dc_loss +
+            self.weight_fpr * fpr_loss
+        )
+        logging.info(f"CE: {ce_loss.item():.4f}, Dice: {dc_loss.item():.4f}, FPR: {fpr_loss.item():.4f}")
         return result
 
 
 class DC_and_topk_loss(nn.Module):
-    def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce=1, weight_dice=1, ignore_label=None):
-        """
-        Weights for CE and Dice do not need to sum to one. You can set whatever you want.
-        :param soft_dice_kwargs:
-        :param ce_kwargs:
-        :param aggregate:
-        :param square_dice:
-        :param weight_ce:
-        :param weight_dice:
-        """
+    def __init__(self,
+                 soft_dice_kwargs,
+                 ce_kwargs,
+                 soft_fpr_kwargs=None,
+                 weight_ce=1,
+                 weight_dice=1,
+                 weight_fpr=0,
+                 ignore_label=None):
+
         super().__init__()
+
         if ignore_label is not None:
             ce_kwargs['ignore_index'] = ignore_label
 
         self.weight_dice = weight_dice
         self.weight_ce = weight_ce
+        self.weight_fpr = weight_fpr
         self.ignore_label = ignore_label
 
         self.ce = TopKLoss(**ce_kwargs)
         self.dc = SoftDiceLoss(apply_nonlin=softmax_helper_dim1, **soft_dice_kwargs)
 
+        soft_fpr_kwargs = soft_fpr_kwargs or {}
+
+        if weight_fpr > 0:
+            self.fpr = SoftFPRLoss(apply_nonlin=softmax_helper_dim1, **soft_fpr_kwargs)
+        else:
+            self.fpr = None
+
     def forward(self, net_output: torch.Tensor, target: torch.Tensor):
-        """
-        target must be b, c, x, y(, z) with c=1
-        :param net_output:
-        :param target:
-        :return:
-        """
+
         if self.ignore_label is not None:
-            assert target.shape[1] == 1, 'ignore label is not implemented for one hot encoded target variables ' \
-                                         '(DC_and_CE_loss)'
+            assert target.shape[1] == 1
             mask = (target != self.ignore_label).bool()
-            # remove ignore label from target, replace with one of the known labels. It doesn't matter because we
-            # ignore gradients in those areas anyway
-            target_dice = torch.clone(target)
-            target_dice[target == self.ignore_label] = 0
+            target_clean = torch.clone(target)
+            target_clean[target == self.ignore_label] = 0
             num_fg = mask.sum()
         else:
-            target_dice = target
+            target_clean = target
             mask = None
+            num_fg = None
 
-        dc_loss = self.dc(net_output, target_dice, loss_mask=mask) \
+        # Dice
+        dc_loss = self.dc(net_output, target_clean, loss_mask=mask) \
             if self.weight_dice != 0 else 0
+
+        # TopK CE
         ce_loss = self.ce(net_output, target) \
             if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
 
-        result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
-        return result
+        # FPR
+        fpr_loss = self.fpr(net_output, target_clean, loss_mask=mask) \
+            if (self.fpr is not None and self.weight_fpr != 0) else 0
+
+        total_loss = (
+            self.weight_ce * ce_loss +
+            self.weight_dice * dc_loss +
+            self.weight_fpr * fpr_loss
+        )
+        logging.info(f"CE: {ce_loss.item():.4f}, Dice: {dc_loss.item():.4f}, FPR: {fpr_loss.item():.4f}")
+        return total_loss
