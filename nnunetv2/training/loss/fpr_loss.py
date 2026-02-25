a44a27c64597bd3d6dbfc7f18af235e526fb5641
entity079 <153630586+entity079@users.noreply.github.com>
2026-02-25 13:54:48 +0530

    Remove print_to_log_file method from SoftFPRLoss
    Removed the print_to_log_file method from SoftFPRLoss class to streamline logging functionality.

--- nnunetv2/training/loss/fpr_loss.py
+++ nnunetv2/training/loss/fpr_loss.py
@@ -8,33 +8,7 @@ import logging
 
 
 class SoftFPRLoss(nn.Module):
-    def print_to_log_file(self, *args, also_print_to_console=True, add_timestamp=True):
-        if self.local_rank == 0:
-            timestamp = time()
-            dt_object = datetime.fromtimestamp(timestamp)
-
-            if add_timestamp:
-                args = (f"{dt_object}:", *args)
-
-            successful = False
-            max_attempts = 5
-            ctr = 0
-            while not successful and ctr < max_attempts:
-                try:
-                    with open(self.log_file, 'a+') as f:
-                        for a in args:
-                            f.write(str(a))
-                            f.write(" ")
-                        f.write("\n")
-                    successful = True
-                except IOError:
-                    print(f"{datetime.fromtimestamp(timestamp)}: failed to log: ", sys.exc_info())
-                    sleep(0.5)
-                    ctr += 1
-            if also_print_to_console:
-                print(*args)
-        elif also_print_to_console:
-            print(*args)
+    
 
     
     def __init__(self, apply_nonlin: Callable = None, batch_dice: bool = False, do_bg: bool = True,
@@ -145,33 +119,6 @@ class MemoryEfficientSoftFPRLoss(nn.Module):
 
         return fpr
 
-def print_to_log_file(self, *args, also_print_to_console=True, add_timestamp=True):
-        if self.local_rank == 0:
-            timestamp = time()
-            dt_object = datetime.fromtimestamp(timestamp)
-
-            if add_timestamp:
-                args = (f"{dt_object}:", *args)
-
-            successful = False
-            max_attempts = 5
-            ctr = 0
-            while not successful and ctr < max_attempts:
-                try:
-                    with open(self.log_file, 'a+') as f:
-                        for a in args:
-                            f.write(str(a))
-                            f.write(" ")
-                        f.write("\n")
-                    successful = True
-                except IOError:
-                    print(f"{datetime.fromtimestamp(timestamp)}: failed to log: ", sys.exc_info())
-                    sleep(0.5)
-                    ctr += 1
-            if also_print_to_console:
-                print(*args)
-        elif also_print_to_console:
-            print(*args)
 
 def get_tp_fp_fn_tn(net_output, gt, axes=None, mask=None, square=False):
     """
