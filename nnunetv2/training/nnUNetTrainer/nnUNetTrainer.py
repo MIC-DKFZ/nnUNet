fc1e08d141c1f3a911d1c3b5134967c9865ababb
Mts027 <luc.bouteille@uk-essen.de>
2026-05-02 17:37:45 +0200

    simpler version

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -747,12 +747,12 @@ class nnUNetTrainer(object):
             ignore_label=self.label_manager.ignore_label)
 
         # validation pipeline
-        val_transforms = None if self.use_fixed_val_tiles else self.get_validation_transforms(
-            deep_supervision_scales,
-            is_cascaded=self.is_cascaded,
-            foreground_labels=self.label_manager.foreground_labels,
-            regions=self.label_manager.foreground_regions if self.label_manager.has_regions else None,
-            ignore_label=self.label_manager.ignore_label)
+        val_transforms = self.get_validation_transforms(deep_supervision_scales,
+                                                        is_cascaded=self.is_cascaded,
+                                                        foreground_labels=self.label_manager.foreground_labels,
+                                                        regions=self.label_manager.foreground_regions if
+                                                        self.label_manager.has_regions else None,
+                                                        ignore_label=self.label_manager.ignore_label)
 
         dataset_tr, dataset_val = self.get_tr_and_val_datasets()
         dl_tr = nnUNetDataLoader(dataset_tr, self.batch_size,
@@ -762,33 +762,31 @@ class nnUNetTrainer(object):
                                  oversample_foreground_percent=self.oversample_foreground_percent,
                                  sampling_probabilities=None, pad_sides=None, transforms=tr_transforms,
                                  probabilistic_oversampling=self.probabilistic_oversampling)
-        dl_val = None if self.use_fixed_val_tiles else nnUNetDataLoader(
-            dataset_val, self.batch_size,
-            self.configuration_manager.patch_size,
-            self.configuration_manager.patch_size,
-            self.label_manager,
-            oversample_foreground_percent=self.oversample_foreground_percent,
-            sampling_probabilities=None, pad_sides=None, transforms=val_transforms,
-            probabilistic_oversampling=self.probabilistic_oversampling)
+        dl_val = nnUNetDataLoader(dataset_val, self.batch_size,
+                                  self.configuration_manager.patch_size,
+                                  self.configuration_manager.patch_size,
+                                  self.label_manager,
+                                  oversample_foreground_percent=self.oversample_foreground_percent,
+                                  sampling_probabilities=None, pad_sides=None, transforms=val_transforms,
+                                  probabilistic_oversampling=self.probabilistic_oversampling)
 
         allowed_num_processes = get_allowed_n_proc_DA()
         if allowed_num_processes == 0:
             mt_gen_train = SingleThreadedAugmenter(dl_tr, None)
-            mt_gen_val = None if self.use_fixed_val_tiles else SingleThreadedAugmenter(dl_val, None)
+            mt_gen_val = SingleThreadedAugmenter(dl_val, None)
         else:
             mt_gen_train = NonDetMultiThreadedAugmenter(data_loader=dl_tr, transform=None,
                                                         num_processes=allowed_num_processes,
                                                         num_cached=max(6, allowed_num_processes // 2), seeds=None,
                                                         pin_memory=self.device.type == 'cuda', wait_time=0.002)
-            mt_gen_val = None if self.use_fixed_val_tiles else NonDetMultiThreadedAugmenter(
-                data_loader=dl_val, transform=None, num_processes=max(1, allowed_num_processes // 2),
-                num_cached=max(3, allowed_num_processes // 4), seeds=None,
-                pin_memory=self.device.type == 'cuda',
-                wait_time=0.002)
+            mt_gen_val = NonDetMultiThreadedAugmenter(data_loader=dl_val,
+                                                      transform=None, num_processes=max(1, allowed_num_processes // 2),
+                                                      num_cached=max(3, allowed_num_processes // 4), seeds=None,
+                                                      pin_memory=self.device.type == 'cuda',
+                                                      wait_time=0.002)
         # # let's get this party started
         _ = next(mt_gen_train)
-        if mt_gen_val is not None:
-            _ = next(mt_gen_val)
+        _ = next(mt_gen_val)
         return mt_gen_train, mt_gen_val
 
     @staticmethod
